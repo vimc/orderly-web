e4b857d5e772e9f0b1e60636f9865b09415e99cc
Alex Hill <alex.hill@gmail.com>
2017-07-12 20:17:51 +0100

    data back in for now

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -2,16 +2,6 @@ set -e
 git_id=$(git rev-parse --short HEAD)
 git_branch=$(git symbolic-ref --short HEAD)
 
-# Run orderly to create demo data
-docker pull docker.montagu.dide.ic.ac.uk:5000/orderly:master
-
-docker run --rm --entrypoint create_orderly_demo.sh \
-    -u ${UID} \
-    -v ${PWD}:./src/app/demo \
-    -w ./src/app/demo \
-    docker.montagu.dide.ic.ac.uk:5000/orderly:master \
-    "./src/app/demo"
-
 # Make the build environment image that is shared between multiple build targets
 ./scripts/make-build-env.sh
 
