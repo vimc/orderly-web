80b442668c88353cb272b497150264b20b4dab45
Alex Hill <alex.hill@gmail.com>
2017-07-12 19:37:36 +0100

    point to demo dir

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -7,8 +7,8 @@ docker pull docker.montagu.dide.ic.ac.uk:5000/orderly:master
 
 docker run --rm --entrypoint create_orderly_demo.sh \
     -u ${UID} \
-    -v ${PWD}:/orderly \
-    -w /orderly \
+    -v ${PWD}:./src/app/demo \
+    -w ./src/app/demo \
     docker.montagu.dide.ic.ac.uk:5000/orderly:master \
     "./src/app/demo"
 
