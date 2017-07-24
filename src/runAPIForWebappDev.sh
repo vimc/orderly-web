6d56e4d2788791059edbfb7a9b5c7f62dec3f0d2
Alex Hill <alex.hill@gmail.com>
2017-07-24 16:19:45 +0100

    no need to generate key pair

--- src/runAPIForWebappDev.sh
+++ src/runAPIForWebappDev.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env bash
 
-# create directory if it doesnt already exist
-if [ ! -d "/etc/montagu/api/token_key" ] 
-then
-	mkdir /etc/montagu/api/token_key
-fi
-
-# create key if it doesnt already exist
-if [ ! -f "/etc/montagu/api/token_key/mykey.pem" ]
-then 
-	openssl genrsa -out /etc/montagu/api/token_key/mykey.pem 512
-fi
-
-# create RSA DER format private key
-if [ ! -f "/etc/montagu/api/token_key/private_key.der" ]
-then 
-openssl pkcs8 -topk8 -inform PEM -outform DER -in  /etc/montagu/api/token_key/mykey.pem -out  /etc/montagu/api/token_key/private_key.der -nocrypt
-fi
-
-# create RSA DER format public key
-if [ ! -f "/etc/montagu/api/token_key/public_key.der" ]
-then 
-openssl rsa -in /etc/montagu/api/token_key/mykey.pem -pubout -outform DER -out /etc/montagu/api/token_key/public_key.der
-fi
-
 if [ ! -f "app/demo/orderly.sqlite" ]
 then
 	./gradlew :app:generateTestData
