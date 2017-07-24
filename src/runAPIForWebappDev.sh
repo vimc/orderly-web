c81c1e95da5a6a402ffc346ec5994dda905a2079
Alex Hill <alex.hill@gmail.com>
2017-07-24 16:32:05 +0100

    copy public key from api

--- src/runAPIForWebappDev.sh
+++ src/runAPIForWebappDev.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+set -ex
 
 if [ ! -f "app/demo/orderly.sqlite" ]
 then
 	./gradlew :app:generateTestData
 fi
 
+# delete directory if it already exists
+if [ -d "/etc/montagu/reports_api/token_key" ] 
+then
+   rm /etc/montagu/reports_api/token_key -r
+fi
+
+mkdir -p /etc/montagu/reports_api/token_key
+cp -R /etc/montagu/api/token_key /etc/montagu/reports_api
+
+ls /etc/montagu/reports_api/token_key
 ./gradlew :run
