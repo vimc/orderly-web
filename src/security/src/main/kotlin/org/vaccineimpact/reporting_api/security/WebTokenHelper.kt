6048d4059d1b0e66bc814e2fb495e42689a02207
Alex Hill <alex.hill@gmail.com>
2017-07-26 12:22:14 +0100

    formatter webtokenhelper

--- src/security/src/main/kotlin/org/vaccineimpact/reporting_api/security/WebTokenHelper.kt
+++ src/security/src/main/kotlin/org/vaccineimpact/reporting_api/security/WebTokenHelper.kt
@@ -4,11 +4,13 @@ import org.vaccineimpact.reporting_api.db.Config
 import java.security.KeyPair
 import java.security.interfaces.RSAPublicKey
 
-class WebTokenHelper(keyPair: KeyPair, val issuerName: String) {
+class WebTokenHelper(keyPair: KeyPair, val issuerName: String)
+{
     val issuer = TokenIssuer(keyPair, issuerName)
     val verifier = TokenVerifier(keyPair.public as RSAPublicKey, issuerName)
 
-    companion object{
-         val oneTimeTokenHelper = WebTokenHelper(KeyHelper.generateKeyPair(), Config["onetime_token.issuer"])
+    companion object
+    {
+        val oneTimeTokenHelper = WebTokenHelper(KeyHelper.generateKeyPair(), Config["onetime_token.issuer"])
     }
 }
