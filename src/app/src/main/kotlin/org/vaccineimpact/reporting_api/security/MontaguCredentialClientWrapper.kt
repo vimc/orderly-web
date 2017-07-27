d9fee532f2a68d9740763f9a7f6f5c662aaed372
Alex <alex.hill@gmail.com>
2017-07-27 11:53:16 +0100

    removed commented code

--- src/app/src/main/kotlin/org/vaccineimpact/reporting_api/security/MontaguCredentialClientWrapper.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/reporting_api/security/MontaguCredentialClientWrapper.kt
@@ -1,5 +1,5 @@
 package org.vaccineimpact.reporting_api.security
-import org.pac4j.core.authorization.authorizer.Authorizer
+
 import org.pac4j.core.client.DirectClient
 import org.pac4j.core.credentials.TokenCredentials
 import org.pac4j.core.profile.CommonProfile
@@ -8,6 +8,5 @@ import org.vaccineimpact.api.models.ErrorInfo
 interface MontaguCredentialClientWrapper
 {
     val errorInfo: ErrorInfo
-    val client : DirectClient<TokenCredentials, CommonProfile>
-  //  val authorisers : List<Authorizer<CommonProfile>>
+    val client: DirectClient<TokenCredentials, CommonProfile>
 }
