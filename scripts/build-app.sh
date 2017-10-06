cb21a37f8f0a1b2845bb4ec891cb1a183255df57
Alex Hill <alex.hill@gmail.com>
2017-10-06 17:33:37 +0100

    no user

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -18,7 +18,6 @@ docker pull docker.montagu.dide.ic.ac.uk:5000/orderly.server:master
 docker run --rm \
     -p 8123:8123 \
     -v $PWD/git:/orderly \
-    --user docker \
     --network=host \
     docker.montagu.dide.ic.ac.uk:5000/orderly.server:master "orderly" &
 
