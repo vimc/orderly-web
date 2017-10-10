3afc68dd10fc82dd0cd0b5899679100dd28a67e6
Alex Hill <alex.hill@gmail.com>
2017-10-10 14:19:40 +0100

    split httpclient into own file

--- src/app/src/main/kotlin/org/vaccineimpact/reporting_api/OrderlyServerAPI.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/reporting_api/OrderlyServerAPI.kt
@@ -40,29 +40,3 @@ class OrderlyServer(config: Config, private val httpClient: HttpClient) : Orderl
         return "$urlBase$url?$queryString"
     }
 }
-
-interface HttpClient
-{
-    fun post(url: String, headers: Map<String, String>, json: Map<String, String> = mapOf()): Response
-    fun get(url: String, headers: Map<String, String>): Response
-}
-
-class KHttpClient : HttpClient
-{
-    override fun post(url: String, headers: Map<String, String>, json: Map<String, String>): Response
-    {
-        return if (json.any())
-        {
-            khttp.post(url, headers, json = json)
-        }
-        else
-        {
-            khttp.post(url, headers)
-        }
-    }
-
-    override fun get(url: String, headers: Map<String, String>): Response
-    {
-        return khttp.get(url, headers)
-    }
-}
