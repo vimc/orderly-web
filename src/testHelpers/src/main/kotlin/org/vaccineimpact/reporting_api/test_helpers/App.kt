35ee2d486cadec7690c25f6ae48b59b0315c73d7
Alex Hill <alex.hill@gmail.com>
2018-08-23 10:43:40 +0100

    remove unused class

--- src/testHelpers/src/main/kotlin/org/vaccineimpact/reporting_api/test_helpers/App.kt
+++
@@ -1,8 +0,0 @@
-//package org.vaccineimpact.reporting_api.test_helpers
-//
-//import java.io.File
-//
-//fun main(args: Array<String>){
-//    Spark.stop()
-//    File(AppConfig()["onetime_token.db.location"]).delete()
-//}
