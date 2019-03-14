93c5ff131e6e9cee5f132aa1725661b381f6b33e
Alex <alex.hill@gmail.com>
2019-03-14 12:48:27 +0000

    simplify key helper for now

--- src/security/src/main/kotlin/org/vaccineimpact/orderlyweb/security/KeyHelper.kt
+++ src/security/src/main/kotlin/org/vaccineimpact/orderlyweb/security/KeyHelper.kt
@@ -2,62 +2,25 @@ package org.vaccineimpact.orderlyweb.security
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.File
 import java.security.*
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.spec.X509EncodedKeySpec
 import java.util.*
 
 object KeyHelper
 {
-    private val keyFactory = KeyFactory.getInstance("RSA")
-    val keyPath = "/etc/orderly/web/token_key"
     private val logger: Logger = LoggerFactory.getLogger(KeyHelper::class.java)
 
-    val keyPair by lazy {
-        if (File(keyPath, "private_key.der").exists())
-        {
-            loadKeyPair()
-        }
-        else
-        {
-            generateKeyPair()
-        }
-    }
-
-    fun loadKeyPair(): KeyPair
-    {
-        logger.info("Loading token signing keypair from $keyPath")
-        return KeyPair(loadPublicKey(), loadPrivateKey())
-    }
-
-    private fun loadPublicKey(): PublicKey
-    {
-        val keyBytes = File(keyPath, "public_key.der").readBytes()
-        val spec = X509EncodedKeySpec(keyBytes)
-
-        return keyFactory.generatePublic(spec)
-    }
-
-    private fun loadPrivateKey(): PrivateKey
-    {
-        val file = File(keyPath, "private_key.der")
-        val keyBytes = file.readBytes()
-        val spec = PKCS8EncodedKeySpec(keyBytes)
-        return keyFactory.generatePrivate(spec)
-    }
+    val keyPair = generateKeyPair()
 
     fun generateKeyPair(): KeyPair
     {
-        logger.info("Unable to find a token keypair at $keyPath. Generating a new")
-        logger.info("RSA keypair for token signing. If other applications need to")
-        logger.info("verify tokens they should use the following public key:")
+        logger.info("Generating a new RSA keypair for token signing.")
+        logger.info("If other applications need to verify tokens they should use the following public key:")
         val generator = KeyPairGenerator.getInstance("RSA").apply {
             initialize(1024)
         }
         val keypair = generator.generateKeyPair()
         val publicKey = Base64.getEncoder().encode(keypair.public.encoded)
-        logger.info("Public key for token verification: " + publicKey)
+        logger.info("Public key for token verification: $publicKey")
         return keypair
     }
 }
