47f8d664571a69d89c8c11c5d93083ffe5be84ac
Alex <alex.hill@gmail.com>
2019-03-14 12:49:31 +0000

    simplify even further

--- src/security/src/main/kotlin/org/vaccineimpact/orderlyweb/security/KeyHelper.kt
+++ src/security/src/main/kotlin/org/vaccineimpact/orderlyweb/security/KeyHelper.kt
@@ -1,26 +1,17 @@
 package org.vaccineimpact.orderlyweb.security
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import java.security.*
-import java.util.*
+import java.security.KeyPair
+import java.security.KeyPairGenerator
 
 object KeyHelper
 {
-    private val logger: Logger = LoggerFactory.getLogger(KeyHelper::class.java)
-
     val keyPair = generateKeyPair()
 
     fun generateKeyPair(): KeyPair
     {
-        logger.info("Generating a new RSA keypair for token signing.")
-        logger.info("If other applications need to verify tokens they should use the following public key:")
         val generator = KeyPairGenerator.getInstance("RSA").apply {
             initialize(1024)
         }
-        val keypair = generator.generateKeyPair()
-        val publicKey = Base64.getEncoder().encode(keypair.public.encoded)
-        logger.info("Public key for token verification: $publicKey")
-        return keypair
+        return generator.generateKeyPair()
     }
 }
