96fad4458408a40075fe49228046fb42d2c7ea99
Alex <alex.hill@gmail.com>
2019-03-15 13:58:20 +0000

    remove unused class

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/errors/FailedLoginError.kt
+++
@@ -1,6 +0,0 @@
-package org.vaccineimpact.orderlyweb.errors
-
-import org.vaccineimpact.orderlyweb.models.ErrorInfo
-
-class FailedLoginError(message: String) : OrderlyWebError(401,
-        listOf(ErrorInfo("login-failed", message)))
