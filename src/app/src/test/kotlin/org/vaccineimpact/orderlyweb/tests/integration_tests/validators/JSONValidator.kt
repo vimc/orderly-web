0a9b1557396d06c795f03784300125ecf23e562f
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-03-15 14:55:38 +0000

    update expected token cookie name

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/validators/JSONValidator.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/validators/JSONValidator.kt
@@ -59,7 +59,7 @@ class JSONValidator
                 expectedErrorText = "Bearer token not supplied in Authorization header, or bearer token was invalid")
         validateError(response,
                 expectedErrorCode = "cookie-bearer-token-invalid",
-                expectedErrorText = "Bearer token not supplied in cookie 'montagu_jwt_token', or bearer token was invalid")
+                expectedErrorText = "Bearer token not supplied in cookie 'orderlyweb_jwt_token', or bearer token was invalid")
         validateError(response,
                 expectedErrorCode = "onetime-token-invalid",
                 expectedErrorText = "Onetime token not supplied, or onetime token was invalid")
