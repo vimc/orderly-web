e12cca53bdcdcc3d33a5a0f04ca5fa6af0161761
Alex <alex.hill@gmail.com>
2019-03-22 17:08:31 +0000

    add routing tests

---
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/RoutingTests.kt
@@ -0,0 +1,54 @@
+package org.vaccineimpact.orderlyweb.tests.integration_tests.tests
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class RoutingTests : IntegrationTest()
+{
+    @Test
+    fun `can get url with or without trailing slash`()
+    {
+        var response = requestHelper.get("/reports/")
+        assertSuccessful(response)
+
+        response = requestHelper.get("/reports")
+        assertSuccessful(response)
+    }
+
+    @Test
+    fun `can get static files`()
+    {
+        val response = requestHelper.getWebPage("/css/style.css", "text/css")
+        assertSuccessful(response)
+        Assertions.assertThat(response.headers["content-type"]).isEqualTo("text/css")
+    }
+
+    @Test
+    fun `gets 404 page if accept header contains html`()
+    {
+        val response = requestHelper.getWebPage("/nonsense", "text/html")
+
+        assertHtmlContentType(response)
+        Assertions.assertThat(response.statusCode).isEqualTo(404)
+    }
+
+    @Test
+    fun `gets 404 page if accept header contains *`()
+    {
+        val response = requestHelper.getWebPage("/nonsense", "*/*")
+
+        assertHtmlContentType(response)
+        Assertions.assertThat(response.statusCode).isEqualTo(404)
+    }
+
+
+    @Test
+    fun `gets json response if accept header does not contain html or *`()
+    {
+        val response = requestHelper.get("/nonsense")
+
+        assertJsonContentType(response)
+        Assertions.assertThat(response.statusCode).isEqualTo(404)
+    }
+
+}
