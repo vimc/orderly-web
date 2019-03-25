beca364142d67909375b08943208e4d7eeb5e54f
Alex <alex.hill@gmail.com>
2019-03-25 16:18:05 +0000

    wip

---
+++ dev/cli.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+image=docker.montagu.dide.ic.ac.uk:5000/montagu-cli:$MONTAGU_API_VERSION
+exec docker run --network dev_default $image "$@"
--- dev/run-dependencies.sh
+++ dev/run-dependencies.sh
@@ -7,7 +7,6 @@ config_path=$(realpath ../config)
 export MONTAGU_API_VERSION=$(<$config_path/api_version)
 export MONTAGU_DB_VERSION=$(<$config_path/db_version)
 export MONTAGU_ORDERLY_SERVER_VERSION=$(<$config_path/orderly_server_version)
-cert_tool_version=master
 
 export TOKEN_KEY_PATH=/etc/montagu/reports_api/token_key
 export MONTAGU_ORDERLY_PATH=$(realpath ../src/app/git)
@@ -17,28 +16,41 @@ echo $MONTAGU_ORDERLY_PATH
 
 export ORDERLY_SERVER_USER_ID=$(id -u $USER)
 
-(
-	cd ../src
-	# get fresh tests data
-	rm app/demo -rf
-	rm app/git -rf
-	./gradlew :generateTestData
-)
-
-# Generate a keypair
-docker run --rm \
-    -v $TOKEN_KEY_PATH:/workspace \
-    docker.montagu.dide.ic.ac.uk:5000/montagu-cert-tool:$cert_tool_version \
-    gen-keypair /workspace
+#(
+#	cd ../src
+#	# get fresh tests data
+#	rm app/demo -rf
+#	rm app/git -rf
+#	./gradlew :generateTestData
+#)
 
 docker-compose pull
 docker-compose up -d
 
+trap cleanup EXIT
+
+docker exec dev_db_1 montagu-wait.sh
+
+export MIGRATE_IMAGE=docker.montagu.dide.ic.ac.uk:5000/montagu-migrate:${MONTAGU_DB_VERSION}
+
+docker pull ${MIGRATE_IMAGE}
+docker run --rm --network=dev_default \
+                     ${MIGRATE_IMAGE} \
+                     migrate
+
+./cli.sh add "Test User" test.user \
+    test.user@example.com password \
+
+./cli.sh addRole test.user user
+./cli.sh addRole test.user admin
+
+docker exec dev_api_1 touch /etc/montagu/api/go_signal
+
 echo "Dependencies are now running; press Ctrl+C to teardown"
 
 # From now on, if the user presses Ctrl+C we should teardown gracefully
-trap on_interrupt INT
-function on_interrupt() {
+trap cleanup INT
+function cleanup() {
 	docker kill dev_orderly_server_1
     docker-compose down
 }
