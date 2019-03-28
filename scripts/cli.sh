1e20628c2569df4155bf071f9dc5e2e67c08f032
Alex <alex.hill@gmail.com>
2019-03-28 18:29:15 +0000

    fix scripts

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 image=docker.montagu.dide.ic.ac.uk:5000/montagu-cli:master
-exec docker run --network montagu_proxy $image "$@"
+exec docker run --network $NETWORK $image "$@"
