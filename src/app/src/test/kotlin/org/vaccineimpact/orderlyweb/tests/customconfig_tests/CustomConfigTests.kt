f69a20333544dbf6559474168e39b22657e556f3
Alex <alex.hill@gmail.com>
2019-03-29 15:30:41 +0000

    remove unused methods

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/customconfig_tests/CustomConfigTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/customconfig_tests/CustomConfigTests.kt
@@ -55,33 +55,9 @@ abstract class CustomConfigTests: MontaguTests()
 
         Assertions.assertThat(response.headers["Content-Encoding"]).isEqualTo("gzip")
     }
-
-    protected fun assertSuccessfulWithResponseText(response: Response)
-    {
-        Assertions.assertThat(response.statusCode)
-                .withFailMessage(response.text)
-                .isEqualTo(200)
-
-        Assertions.assertThat(response.headers["Content-Encoding"]).isEqualTo("gzip")
-    }
-
-    protected fun assertUnauthorized(response: Response, reportName: String)
-    {
-        Assertions.assertThat(response.statusCode)
-                .isEqualTo(403)
-
-        JSONValidator.validateError(response.text, "forbidden",
-                "You do not have sufficient permissions to access this resource. Missing these permissions: report:${reportName}/reports.read")
-
-    }
-
     protected fun assertJsonContentType(response: Response)
     {
         Assertions.assertThat(response.headers["content-type"]).isEqualTo("application/json;charset=utf-8")
     }
 
-    protected fun assertHtmlContentType(response: Response)
-    {
-        Assertions.assertThat(response.headers["content-type"]).isEqualTo("text/html")
-    }
 }
