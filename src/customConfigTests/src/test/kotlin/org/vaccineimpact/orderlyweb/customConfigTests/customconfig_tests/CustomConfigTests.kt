db0733b0f7ca2d2dbf21629442116781da8a0ee8
Alex <alex.hill@gmail.com>
2019-04-01 15:39:18 +0100

    Revert "remove submodule"
    This reverts commit f5d4c7fd6a8c883276c49c9f59206842c1a001af.

---
+++ src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/customconfig_tests/CustomConfigTests.kt
@@ -0,0 +1,74 @@
+package org.vaccineimpact.orderlyweb.customConfigTests.customconfig_tests
+
+import khttp.responses.Response
+import org.assertj.core.api.Assertions
+import org.junit.After
+import org.junit.Before
+import org.vaccineimpact.orderlyweb.app_start.main
+import org.vaccineimpact.orderlyweb.db.AppConfig
+import org.vaccineimpact.orderlyweb.db.getResource
+import org.vaccineimpact.orderlyweb.test_helpers.MontaguTests
+import java.io.File
+
+abstract class CustomConfigTests : MontaguTests()
+{
+    var appRunning: Boolean = false
+
+    fun startApp(customConfig: String)
+    {
+        val localConfig = File("local")
+        localConfig.createNewFile()
+        localConfig.writeText(customConfig)
+
+        AppConfig.properties.apply {
+            localConfig.inputStream().use { load(it) }
+        }
+
+        while (appRunning)
+        {
+            Thread.sleep(500)
+        }
+        appRunning = true
+        main(emptyArray())
+        Thread.sleep(500)
+    }
+
+    @Before
+    fun createDatabase()
+    {
+        println("Copying database from: ${AppConfig()["db.template"]}")
+
+        val newDbFile = File(AppConfig()["db.location"])
+        val source = File(AppConfig()["db.template"])
+
+        source.copyTo(newDbFile, true)
+        Thread.sleep(1000)
+    }
+
+    @After
+    fun cleanup()
+    {
+        File(AppConfig()["db.location"]).delete()
+        File("local").delete()
+        spark.Spark.stop()
+
+        // reset the properties
+        AppConfig.properties.apply {
+            load(getResource("config.properties").openStream())
+            val global = File("/etc/orderly/web/config.properties")
+            if (global.exists())
+            {
+                global.inputStream().use { load(it) }
+            }
+        }
+    }
+
+    protected fun assertSuccessful(response: Response)
+    {
+        Assertions.assertThat(response.statusCode)
+                .isEqualTo(200)
+
+        Assertions.assertThat(response.headers["Content-Encoding"]).isEqualTo("gzip")
+    }
+
+}
---
+++ src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/customconfig_tests/RequestHelper.kt
@@ -0,0 +1,36 @@
+package org.vaccineimpact.orderlyweb.customConfigTests.customconfig_tests
+
+import khttp.responses.Response
+import org.vaccineimpact.orderlyweb.ContentTypes
+import org.vaccineimpact.orderlyweb.db.AppConfig
+import org.vaccineimpact.orderlyweb.security.WebTokenHelper
+
+class RequestHelper
+{
+    val apiBaseUrl: String = "http://localhost:${AppConfig()["app.port"]}/api/v1"
+
+    fun get(url: String, contentType: String = ContentTypes.json,
+            userEmail: String): Response
+    {
+        val token = generateToken(userEmail)
+        val headers = standardHeaders(contentType).withAuthorizationHeader(token)
+        return get(apiBaseUrl + url, headers)
+    }
+
+    private fun standardHeaders(contentType: String): Map<String, String>
+    {
+        return mapOf(
+                "Accept" to contentType,
+                "Accept-Encoding" to "gzip"
+        )
+    }
+
+    private fun Map<String, String>.withAuthorizationHeader(token: String) = this +
+            mapOf("Authorization" to "Bearer $token")
+
+    private fun get(url: String, headers: Map<String, String>) = khttp.get(url, headers)
+
+    private fun generateToken(emailAddress: String) =
+            WebTokenHelper.instance.issuer.generateBearerToken(emailAddress)
+
+}
