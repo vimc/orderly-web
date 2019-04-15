62079363ff77ae35bce7b933b75a44a9af480be5
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-15 15:57:17 +0100

    OrderlyWebIndirectClient has dummy CredentialsExtractor and Authenticator as it delegates these functions to external auth providers

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/clients/OrderlyWebIndirectClient.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/clients/OrderlyWebIndirectClient.kt
@@ -1,15 +1,14 @@
 package org.vaccineimpact.orderlyweb.security.clients
 
 import org.pac4j.core.client.IndirectClient
+import org.pac4j.core.context.WebContext
 import org.pac4j.core.credentials.TokenCredentials
-import org.pac4j.core.credentials.extractor.FormExtractor
+import org.pac4j.core.credentials.authenticator.Authenticator
+import org.pac4j.core.credentials.extractor.CredentialsExtractor
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.core.redirect.RedirectAction
-import org.pac4j.http.client.indirect.FormClient
-import org.pac4j.http.credentials.extractor.CookieExtractor
 import org.vaccineimpact.orderlyweb.db.AppConfig
 import org.vaccineimpact.orderlyweb.db.OrderlyUserRepository
-import org.vaccineimpact.orderlyweb.models.ErrorInfo
 import org.vaccineimpact.orderlyweb.security.authentication.MontaguAuthenticator
 import org.vaccineimpact.orderlyweb.security.providers.khttpMontaguAPIClient
 
@@ -22,13 +21,34 @@ class OrderlyWebIndirectClient() : IndirectClient<TokenCredentials, CommonProfil
 
     override fun clientInit()
     {
-        defaultCredentialsExtractor(CookieExtractor("montagu_jwt_token"))
+        defaultCredentialsExtractor(OrderlyWebCredentialsExtractor())
+
+        //TODO: When implementing https://vimc.myjetbrains.com/youtrack/issue/mrc-228 we might put logic to choose
+        //to redirect to weblogin or to configured Montagu homepage (skip our landing page for Montagu users)
         defaultRedirectActionBuilder {
             RedirectAction.redirect(AppConfig()["app.url"] + "/weblogin")
         }
 
-        defaultAuthenticator(MontaguAuthenticator(OrderlyUserRepository(), khttpMontaguAPIClient()))
+        defaultAuthenticator(OrderlyWebAuthenticator())
     }
 
 
 }
+
+//The OrderlyIndirectClient is only used to forward the user on to the appropriate auth provider via the landing page.
+//We need to provide a CredentialsExtractor object to keep pac4j happy, but it should never actually be invoked, so this
+//is really just a dummy class. Similarly for OrderlyWebAuthenticator
+class OrderlyWebCredentialsExtractor: CredentialsExtractor<TokenCredentials> {
+
+    override fun extract(context: WebContext) :TokenCredentials
+    {
+        throw UnsupportedOperationException("OrderlyWebCredentialsExtractor should not be invoked.")
+    }
+}
+
+class OrderlyWebAuthenticator: Authenticator<TokenCredentials> {
+    override fun validate(credentials: TokenCredentials, context: WebContext) {
+        throw UnsupportedOperationException("OrderlyWebAuthenticator should not be invoked.")
+    }
+}
+
