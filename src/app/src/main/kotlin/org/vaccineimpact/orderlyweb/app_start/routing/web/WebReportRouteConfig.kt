f4f071c1b80f303bf3f641afdcafd96e28e14780
Alex <alex.hill@gmail.com>
2019-04-15 19:00:09 +0100

    reuse api controller for web route

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/app_start/routing/web/WebReportRouteConfig.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/app_start/routing/web/WebReportRouteConfig.kt
@@ -2,6 +2,7 @@ package org.vaccineimpact.orderlyweb.app_start.routing.web
 
 import org.vaccineimpact.orderlyweb.WebEndpoint
 import org.vaccineimpact.orderlyweb.app_start.RouteConfig
+import org.vaccineimpact.orderlyweb.controllers.api.ArtefactController
 import org.vaccineimpact.orderlyweb.controllers.web.ReportController
 import org.vaccineimpact.orderlyweb.secure
 
@@ -11,6 +12,9 @@ object WebReportRouteConfig : RouteConfig
     override val endpoints = listOf(
             WebEndpoint("/reports/:name/:version/",
                     ReportController::class, "get")
+                    .secure(readReports),
+            WebEndpoint("/reports/:name/versions/:version/artefacts/:artefact/",
+                    ArtefactController::class, "download")
                     .secure(readReports)
     )
 }
--- src/app/templates/partials/report.ftl
+++ src/app/templates/partials/report.ftl
@@ -2,5 +2,11 @@
 <h1 class="h2">${report.name}</h1>
 <p class="small text-muted">${report.id}</p>
 
-<iframe src="/api/v1/reports/${report.name}/versions/${report.id}/artefacts/${report.artefacts[0].files[0]}"
-        width="100%" height="600px" class="border border-dark p-3"/>
+<iframe src="/reports/${report.name}/versions/${report.id}/artefacts/${report.artefacts[0].files[0]}?inline=true"
+        width="100%" height="600px" class="border border-dark p-3"></iframe>
+
+<div class="text-right">
+    <a target="_blank" href="/reports/${report.name}/versions/${report.id}/artefacts/${report.artefacts[0].files[0]}?inline=true">
+        View fullscreen
+    </a>
+</div>
