72466ceda954266b4a886c5ee85c9f6863c42c91
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-16 17:15:04 +0100

    Added SecurityController tests

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/controllers/web/SecurityController.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/controllers/web/SecurityController.kt
@@ -2,16 +2,10 @@ package org.vaccineimpact.orderlyweb.controllers.web
 
 import org.vaccineimpact.orderlyweb.ActionContext
 import org.vaccineimpact.orderlyweb.controllers.Controller
-import org.vaccineimpact.orderlyweb.db.Orderly
-import org.vaccineimpact.orderlyweb.db.OrderlyClient
-import org.vaccineimpact.orderlyweb.security.authentication.AuthenticationConfig
 import org.vaccineimpact.orderlyweb.viewmodels.AppViewModel
 
-class SecurityController(actionContext: ActionContext,
-                     private val orderly: OrderlyClient) : Controller(actionContext)
+class SecurityController(actionContext: ActionContext) : Controller(actionContext)
 {
-    constructor(actionContext: ActionContext) : this(actionContext, Orderly())
-
     class WebloginViewModel(context: ActionContext, val requestedUrl: String) : AppViewModel(context)
 
     @Template("weblogin.ftl")
