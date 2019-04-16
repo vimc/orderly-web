bc64393d70428ef462395693723289e5691d6162
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-16 17:20:49 +0100

    Do not use master image of cli - it uses master of the other images too

--- dev/cli.sh
+++ dev/cli.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-image=docker.montagu.dide.ic.ac.uk:5000/orderly-web-cli:master
+git_id=$(git rev-parse --short=7 HEAD)
+
+image=docker.montagu.dide.ic.ac.uk:5000/orderly-web-cli:$git_id
 docker run -v $PWD/src/app/demo:/orderly $image "$@"
