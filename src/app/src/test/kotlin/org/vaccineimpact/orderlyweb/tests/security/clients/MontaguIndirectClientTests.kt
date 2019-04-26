dbae1da1a97b0133d59765f76f95d45c3ab5be98
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-26 10:42:52 +0100

    Fixed cookie name typo

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/security/clients/MontaguIndirectClientTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/security/clients/MontaguIndirectClientTests.kt
@@ -22,24 +22,19 @@ class MontaguIndirectClientTests : TeamcityTests()
         val sut = MontaguIndirectClient()
         sut.init()
 
-        //inherited private credentialsExtractor field should be CookieExtractor
         val extractor = sut.credentialsExtractor
         assertThat(extractor is CookieExtractor).isTrue()
-        assertThat(extractor.toString()).isEqualTo("montagu_jwt_token")
+        assertThat(extractor.toString()).contains("cookieName: montagu_jwt_token")
 
-        //inherited private authenticator field should be MontaguAuthenticator
         val authenticator = sut.authenticator
         assertThat(authenticator is MontaguAuthenticator).isTrue()
 
-        //inherited private authorizationGenerators field should include OrderlyAuthorizationGenerator
         val ags = sut.authorizationGenerators
         Assertions.assertThat((ags as List<AuthorizationGenerator<CommonProfile>>).count()).isEqualTo(1)
         assertThat(ags[0] is OrderlyAuthorizationGenerator).isTrue()
 
-        //inherited private callbackUrl field should be set
         assertThat(sut.callbackUrl).isEqualTo("/login")
 
-        //inherited logoutActionBuilder should be MontaguLogoutActionBuilder
         assertThat(sut.logoutActionBuilder is MontaguLogoutActionBuilder).isTrue()
 
     }
