839e4d77c5550ce18772390d52ab2f475870166d
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-26 16:22:01 +0100

    Added test for rendering index template

---
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/templates/indexTests.kt
@@ -0,0 +1,45 @@
+package org.vaccineimpact.orderlyweb.tests.unit_tests.templates
+
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.Rule
+import org.junit.Test
+import org.vaccineimpact.orderlyweb.app_start.WebErrorHandler
+import org.vaccineimpact.orderlyweb.controllers.web.HomeController
+import org.vaccineimpact.orderlyweb.models.Report
+import org.vaccineimpact.orderlyweb.test_helpers.TeamcityTests
+import org.vaccineimpact.orderlyweb.tests.unit_tests.templates.rules.FreemarkerTestRule
+import org.xmlmatchers.XmlMatchers
+
+class indexTests: TeamcityTests()
+{
+    @get:Rule
+    val template = FreemarkerTestRule("index.ftl")
+
+    @Test
+    fun `renders correctly`()
+    {
+        val mockModel = mock<HomeController.IndexViewModel> {
+            on { appName } doReturn "testApp"
+            on { reports } doReturn listOf(Report("r1", "r1 display", "v1"),
+                                            Report("r2", "r2 display", "v2"))
+        }
+
+        val xmlResponse = template.xmlResponseFor(mockModel)
+
+        assertThat(xmlResponse, XmlMatchers.hasXPath("//h1/text()", equalToIgnoringWhiteSpace("All reports")))
+
+        assertThat(xmlResponse, XmlMatchers.hasXPath("//div[@id='content']/a[1]/text()",
+                equalToIgnoringWhiteSpace("r1")))
+        assertThat(xmlResponse, XmlMatchers.hasXPath("//div[@id='content']/a[1]/@href", equalTo("reports/r1/v1")))
+
+        assertThat(xmlResponse, XmlMatchers.hasXPath("//div[@id='content']/a[2]/text()",
+                equalToIgnoringWhiteSpace("r2")))
+        assertThat(xmlResponse, XmlMatchers.hasXPath("//div[@id='content']/a[2]/@href", equalTo("reports/r2/v2")))
+
+    }
+}
+
+
