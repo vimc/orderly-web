e374dab66f7a182a758dc5ffcd1779fbe48812d4
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-06-13 14:50:35 +0100

    Added error classes tests

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/OrderlyWebErrorTests.kt
+++
@@ -1,8 +0,0 @@
-package org.vaccineimpact.orderlyweb.tests.unit_tests
-
-import org.vaccineimpact.orderlyweb.test_helpers.TeamcityTests
-
-class OrderlyWebErrorTests: TeamcityTests()
-{
-
-}
---
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/errors/ExceptionTests.kt
@@ -0,0 +1,29 @@
+package org.vaccineimpact.orderlyweb.tests.unit_tests.errors
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import org.vaccineimpact.orderlyweb.errors.*
+import org.vaccineimpact.orderlyweb.test_helpers.TeamcityTests
+import java.security.Permission
+
+class ExceptionTests: TeamcityTests()
+{
+    @Test
+    fun `can create UnsupportedValueException`()
+    {
+        val sut = UnsupportedValueException("test")
+        Assertions.assertThat(sut.message).isEqualTo(
+                "Unsupported value 'test' of type 'String'")
+
+    }
+
+    @Test
+    fun `can create PermisionRequirementParseException`()
+    {
+        val sut = PermissionRequirementParseException("test bad permission")
+        Assertions.assertThat(sut.message).isEqualTo(
+                "Unable to parse 'test bad permission' as a PermissionRequirement. " +
+                        "It should have the form 'scope/name' where scope is either the global scope '*' " +
+                        "or a specific scope identifier in the form 'prefix:<urlKey>'")
+    }
+}
