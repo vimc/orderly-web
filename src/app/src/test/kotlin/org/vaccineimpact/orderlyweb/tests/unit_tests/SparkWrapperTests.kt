6951025bc33ee710258a7a95a9182c07b38e53f5
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-06-14 10:59:00 +0100

    Took out suspicious SparkWrapper tests

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/SparkWrapperTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/SparkWrapperTests.kt
@@ -10,37 +10,9 @@ import spark.ResponseTransformer
 import spark.Route
 import spark.route.HttpMethod
 
-class JoinPathTests: TeamcityTests()
+class SparkWrapperTests: TeamcityTests()
 {
     private val mockRoute = mock<Route>()
     private val mockTransformer = mock<ResponseTransformer>()
 
-    @Test
-    fun `can map all Http methods without error`()
-    {
-        val sut = SparkServiceWrapper()
-
-        sut.map("/get", HttpMethod.get, "*/*", mockRoute, null)
-        sut.map("/post", HttpMethod.post, "*/*", mockRoute, null)
-        sut.map("/put", HttpMethod.put, "*/*", mockRoute, null)
-        sut.map("/patch", HttpMethod.patch, "*/*", mockRoute, null)
-        sut.map("/delete", HttpMethod.delete, "*/*", mockRoute, null)
-
-        sut.map("/get2", HttpMethod.get, "*/*", mockRoute, mockTransformer)
-        sut.map("/post2", HttpMethod.post, "*/*", mockRoute, mockTransformer)
-        sut.map("/put2", HttpMethod.put, "*/*", mockRoute, mockTransformer)
-        sut.map("/patch2", HttpMethod.patch, "*/*", mockRoute, mockTransformer)
-        sut.map("/delete2", HttpMethod.delete, "*/*", mockRoute, mockTransformer)
-    }
-
-    @Test
-    fun `throws UnsupportedValueException if map unexpected Http method`()
-    {
-        val sut = SparkServiceWrapper()
-        assertThatThrownBy{ sut.map("/options", HttpMethod.options, "*/*", mockRoute, null) }
-                .isInstanceOf(UnsupportedValueException::class.java)
-
-        assertThatThrownBy{ sut.map("/options", HttpMethod.options, "*/*", mockRoute, mockTransformer) }
-                .isInstanceOf(UnsupportedValueException::class.java)
-    }
 }
