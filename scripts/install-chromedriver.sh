131e6962b9d4007c41e60306a0e9223a3d71aafc
Alex <alex.hill@gmail.com>
2019-08-05 18:31:31 +0100

    update chromedriver

--- scripts/install-chromedriver.sh
+++ scripts/install-chromedriver.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
 
-wget https://chromedriver.storage.googleapis.com/2.46/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/76.0.3809.68/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
