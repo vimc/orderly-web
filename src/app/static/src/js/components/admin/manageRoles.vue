a22582070350a7fbd8e682c6fb619b73ea37de80
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-01-21 16:41:08 +0000

    Acutally add component

--- src/app/static/src/js/components/admin/manageRoles.vue
+++ src/app/static/src/js/components/admin/manageRoles.vue
@@ -1,13 +1,38 @@
 <template>
-    $END$
+    <div id="role-list">
+        <role-list :can-remove-members="true"
+                   :can-remove-roles="false"
+                   :roles="roles"></role-list>
+    </div>
 </template>
 
 <script>
+    import {api} from "../../utils/api";
+    import UserList from "../permissions/userList.vue";
+    import RoleList from "../permissions/roleList.vue";
+
     export default {
-        name: "manageRoles"
-    }
+        name: 'manageRoles',
+        mounted() {
+            this.getRoles();
+
+        },
+        data() {
+            return {
+                roles: []
+            }
+        },
+        methods: {
+            getRoles: function () {
+                api.get(`/roles/all/`)
+                    .then(({data}) => {
+                        this.roles = data.data
+                    })
+            }
+        },
+        components: {
+            RoleList,
+            UserList
+        }
+    };
 </script>
-
-<style scoped>
-
-</style>
