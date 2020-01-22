9bfd88036ad0940336e38cf6befa7b7b7d91bbaf
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-01-22 17:40:07 +0000

    Role widget: add users

---
+++ src/app/static/src/js/components/admin/addUserToRole.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="mb-3 ml-3">
+        <vue-bootstrap-typeahead
+                size="sm"
+                v-model="newUser"
+                :placeholder="email"
+                :data="availableUsers">
+            <template slot="append">
+                <button v-on:click="add" type="submit" class="btn btn-sm">Add user</button>
+            </template>
+        </vue-bootstrap-typeahead>
+        <error-info :default-message="defaultMessage" :api-error="error"></error-info>
+    </div>
+</template>
+
+<script>
+    import {api} from "../../utils/api";
+    import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+    import ErrorInfo from "../errorInfo.vue";
+
+    export default {
+        name: 'addUserToRole',
+        props: ['role', 'availableUsers'],
+        data() {
+            return {
+                newUser: "",
+                error: "",
+                defaultMessage: ""
+            }
+        },
+        components: {
+            ErrorInfo,
+            VueBootstrapTypeahead
+        },
+        watch: {
+            newUserGroup() {
+                this.error = ""
+            }
+        },
+        methods: {
+            add: function () {
+                if (!new Set(this.availableUsers).has(this.newUser)) {
+                    const msg = `${this.newUser} is not an available user or already belongs to role`;
+                    this.error = msg;
+                    this.defaultMessage = msg;
+                    return;
+                }
+
+                const data = {
+                    email: this.newUser
+                };
+
+                api.post(`/user-groups/${encodeURIComponent(this.role)}/`, data)
+                    .then(() => {
+                        this.$emit("added");
+                        this.newUser = "";
+                        this.error = null;
+                    })
+                    .catch((error) => {
+                        this.error = error;
+                        this.defaultMessage = `could not add user`;
+                    });
+            }
+        }
+    };
+</script>
