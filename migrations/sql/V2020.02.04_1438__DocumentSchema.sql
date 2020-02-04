ae32eda255e8a7e539fe8abe5c0e5bb3f0e6f905
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-02-04 14:57:32 +0000

    Fix errors

--- migrations/sql/V2020.02.04_1438__DocumentSchema.sql
+++ migrations/sql/V2020.02.04_1438__DocumentSchema.sql
@@ -1,14 +1,16 @@
-CREATE TABLE "orderlyweb_document"(
+CREATE TABLE "orderlyweb_document_dir" (
+    "path" TEXT,
+    "parent" TEXT,
+    PRIMARY KEY ("path"),
+    FOREIGN KEY ("parent") REFERENCES "orderlyweb_document_dir" ("path")
+);
+
+CREATE TABLE "orderlyweb_document" (
     "id" INTEGER PRIMARY KEY NOT NULL,
     "filename" TEXT NOT NULL,
     "show" INTEGER DEFAULT 1,
     "display_name" TEXT,
     "description" TEXT,
-    FOREIGN KEY ("parent") REFERENCES "orderlyweb_document_dir" ("path") NOT NULL
-)
-
-CREATE TABLE "orderlyweb_document_dir"(
-    "path" TEXT,
-    PRIMARY KEY ("path"),
+    "parent" TEXT NOT NULL,
     FOREIGN KEY ("parent") REFERENCES "orderlyweb_document_dir" ("path")
-)
+);
