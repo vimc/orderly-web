f4868a2562bce7b70d9ed18ad1e4c9d4f5c03c74
Alex <alex.hill@gmail.com>
2020-02-05 14:25:47 +0000

    extra controller tests

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/DocumentControllerTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/DocumentControllerTests.kt
@@ -3,20 +3,23 @@ package org.vaccineimpact.orderlyweb.tests.unit_tests.controllers.web
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.After
 import org.junit.Test
 import org.vaccineimpact.orderlyweb.ActionContext
 import org.vaccineimpact.orderlyweb.Files
 import org.vaccineimpact.orderlyweb.controllers.web.DocumentController
 import org.vaccineimpact.orderlyweb.db.AppConfig
-import org.vaccineimpact.orderlyweb.test_helpers.TeamcityTests
+import org.vaccineimpact.orderlyweb.errors.MissingParameterError
+import org.vaccineimpact.orderlyweb.errors.OrderlyFileNotFoundError
 import org.vaccineimpact.orderlyweb.tests.unit_tests.controllers.api.ControllerTest
 import java.io.File
 
 class DocumentControllerTests : ControllerTest()
 {
     @After
-    fun cleanup() {
+    fun cleanup()
+    {
         File("documents").deleteRecursively()
     }
 
@@ -36,5 +39,24 @@ class DocumentControllerTests : ControllerTest()
         verify(mockContext)
                 .addResponseHeader("Content-Disposition", "attachment; filename=\"some/path/file.csv\"")
         verify(mockContext).addDefaultResponseHeaders("text/csv")
+    }
+
+    @Test
+    fun `error is thrown if the path is missing`()
+    {
+        val sut = DocumentController(mock(), AppConfig(), Files())
+        assertThatThrownBy { sut.getDocument() }.isInstanceOf(MissingParameterError::class.java)
+    }
+
+    @Test
+    fun `error is thrown if the file does not exist`()
+    {
+        val mockContext = mock<ActionContext>() {
+            on { splat() } doReturn arrayOf("some", "path", "file.csv")
+            on { this.getSparkResponse() } doReturn mockSparkResponse
+        }
+
+        val sut = DocumentController(mockContext, AppConfig(), Files())
+        assertThatThrownBy { sut.getDocument() }.isInstanceOf(OrderlyFileNotFoundError::class.java)
     }
 }
