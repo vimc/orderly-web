c86d6a1692ab130ac2b13f95066d67e5a105762f
Alex <alex.hill@gmail.com>
2020-02-05 14:30:33 +0000

    verify inline behaviour

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/DocumentControllerTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/DocumentControllerTests.kt
@@ -1,11 +1,10 @@
 package org.vaccineimpact.orderlyweb.tests.unit_tests.controllers.web
 
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.After
 import org.junit.Test
+import org.mockito.internal.verification.Times
 import org.vaccineimpact.orderlyweb.ActionContext
 import org.vaccineimpact.orderlyweb.Files
 import org.vaccineimpact.orderlyweb.controllers.web.DocumentController
@@ -39,6 +38,24 @@ class DocumentControllerTests : ControllerTest()
         verify(mockContext)
                 .addResponseHeader("Content-Disposition", "attachment; filename=\"some/path/file.csv\"")
         verify(mockContext).addDefaultResponseHeaders("text/csv")
+    }
+
+    @Test
+    fun `attachment header is not added for inline documents`()
+    {
+        File("documents/some/path").mkdirs()
+        File("documents/some/path/file.csv").createNewFile()
+
+        val mockContext = mock<ActionContext>() {
+            on { splat() } doReturn arrayOf("some", "path", "file.csv")
+            on { queryParams("inline") } doReturn "true"
+            on { this.getSparkResponse() } doReturn mockSparkResponse
+        }
+
+        val sut = DocumentController(mockContext, AppConfig(), Files())
+        sut.getDocument()
+        verify(mockContext, Times(0))
+                .addResponseHeader(eq("Content-Disposition"), any())
     }
 
     @Test
