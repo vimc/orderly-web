2d83748e7b9fceb81c29d1765c20bed5dbb59cef
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-02-07 09:26:56 +0000

    Added endpoint route

---
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/app_start/routing/api/DocumentRouteConfig.kt
@@ -0,0 +1,18 @@
+package org.vaccineimpact.orderlyweb.app_start.routing.api
+
+import org.vaccineimpact.orderlyweb.*
+import org.vaccineimpact.orderlyweb.app_start.RouteConfig
+import org.vaccineimpact.orderlyweb.controllers.api.DocumentController
+import spark.route.HttpMethod
+
+object DocumentRouteConfig : RouteConfig
+{
+    //TODO: Should have a documents.manage permission instead?
+    private val refreshPermission = setOf("*/users.manage")
+    private val controller = DocumentController::class
+
+    override val endpoints: List<EndpointDefinition> = listOf(
+            APIEndpoint("/documents/refresh/", controller, "refresh", method = HttpMethod.post)
+                    .secure(refreshPermission)
+    )
+}
