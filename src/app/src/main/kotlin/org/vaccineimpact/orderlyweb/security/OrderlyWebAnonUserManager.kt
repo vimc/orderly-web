ddd883cb2b364b8a54ec9c8747ab9e591d25a885
Alex <alex.hill@gmail.com>
2020-03-09 12:22:58 +0000

    logging

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/OrderlyWebAnonUserManager.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/OrderlyWebAnonUserManager.kt
@@ -5,6 +5,7 @@ import org.pac4j.core.config.Config
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.core.profile.ProfileManager
 import org.pac4j.sparkjava.SparkWebContext
+import org.slf4j.LoggerFactory
 import org.vaccineimpact.orderlyweb.db.AuthorizationRepository
 import org.vaccineimpact.orderlyweb.db.OrderlyAuthorizationRepository
 import org.vaccineimpact.orderlyweb.models.permissions.PermissionSet
@@ -12,19 +13,21 @@ import org.vaccineimpact.orderlyweb.security.authorization.orderlyWebPermissions
 import org.vaccineimpact.orderlyweb.security.clients.OrderlyWebIndirectClient
 import java.util.*
 
-interface AnonUserManager {
+interface AnonUserManager
+{
     fun updateProfile(context: SparkWebContext?,
                       config: Config?,
                       clients: String?)
 }
+
 class OrderlyWebAnonUserManager(
-        private val authRepo: AuthorizationRepository = OrderlyAuthorizationRepository()): AnonUserManager
+        private val authRepo: AuthorizationRepository = OrderlyAuthorizationRepository()) : AnonUserManager
 {
     private val clientFinder = DefaultSecurityClientFinder()
-
+    val logger = LoggerFactory.getLogger("OrderlyWebAnonUserManager")
     override fun updateProfile(context: SparkWebContext?,
-                      config: Config?,
-                      clients: String?)
+                               config: Config?,
+                               clients: String?)
     {
         val client = clientFinder.find(config?.clients, context, clients).first()
         val manager = ProfileManager<CommonProfile>(context)
@@ -32,16 +35,19 @@ class OrderlyWebAnonUserManager(
 
         if (currentProfile.isPresent && currentProfile.get().id != "anon")
         {
+            logger.info("there is a user logged in")
             // a user is logged in, so leave their profile as is
             return
         }
 
         if (client is OrderlyWebIndirectClient)
         {
+            logger.info("adding or updating anon profile")
             addOrUpdateAnonProfile(currentProfile, manager)
         }
         else
         {
+            logger.info("external client, so removing anon profile")
             manager.remove(true)
         }
     }
@@ -53,6 +59,7 @@ class OrderlyWebAnonUserManager(
 
         if (!currentProfile.isPresent)
         {
+            logger.info("adding anon")
             val profile = CommonProfile().apply {
                 id = "anon"
                 orderlyWebPermissions = permissions
@@ -62,6 +69,7 @@ class OrderlyWebAnonUserManager(
         }
         else
         {
+            logger.info("updating anon permissions")
             currentProfile.get().orderlyWebPermissions = permissions
         }
     }
--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/OrderlyWebSecurityLogic.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/security/OrderlyWebSecurityLogic.kt
@@ -4,9 +4,13 @@ import org.pac4j.core.config.Config
 import org.pac4j.core.engine.DefaultSecurityLogic
 import org.pac4j.core.engine.SecurityGrantedAccessAdapter
 import org.pac4j.core.http.adapter.HttpActionAdapter
+import org.pac4j.core.profile.CommonProfile
+import org.pac4j.core.profile.ProfileManager
 import org.pac4j.sparkjava.SparkWebContext
+import org.slf4j.LoggerFactory
 import org.vaccineimpact.orderlyweb.security.authentication.AuthenticationConfig
 import org.vaccineimpact.orderlyweb.security.authentication.OrderlyWebAuthenticationConfig
+import kotlin.math.log
 
 class OrderlyWebSecurityLogic(private val authenticationConfig: AuthenticationConfig = OrderlyWebAuthenticationConfig(),
                               private val anonUserManager: AnonUserManager = OrderlyWebAnonUserManager())
@@ -22,9 +26,22 @@ class OrderlyWebSecurityLogic(private val authenticationConfig: AuthenticationCo
                          inputMultiProfile: Boolean?,
                          vararg parameters: Any?): Any?
     {
+        logger.info("OW security logic being called")
         if (authenticationConfig.allowAnonUser)
         {
+            logger.info("anon user is enabled in config")
             anonUserManager.updateProfile(context, config, clients)
+        }
+
+        val client = clientFinder.find(config?.clients, context, clients).first()
+        val manager = ProfileManager<CommonProfile>(context)
+        val currentProfile = manager.get(true)
+        if (currentProfile.isPresent)
+        {
+            logger.info("currentprofile: ${currentProfile.get().id}")
+        }
+        else {
+            logger.info("no profile in context")
         }
         return super.perform(context,
                 config,
