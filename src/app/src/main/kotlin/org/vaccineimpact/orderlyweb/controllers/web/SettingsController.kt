e47533c85b1d9c7e8e8ed9276289934c0730a2fe
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-04-01 12:21:03 +0100

    Update setting in db

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/controllers/web/SettingsController.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/controllers/web/SettingsController.kt
@@ -2,14 +2,22 @@ package org.vaccineimpact.orderlyweb.controllers.web
 
 import org.vaccineimpact.orderlyweb.ActionContext
 import org.vaccineimpact.orderlyweb.controllers.Controller
+import org.vaccineimpact.orderlyweb.db.repositories.*
 
-class SettingsController(context: ActionContext) : Controller(context)
+class SettingsController(context: ActionContext,
+                         private val repo: SettingsRepository) : Controller(context)
 {
+    constructor(context: ActionContext) : this(context,
+            OrderlySettingsRepository())
+
     fun getAuthAllowGuest(): Boolean
     {
-        return false;
+        return repo.getAuthAllowGuest()
     }
 
     fun setAuthAllowGuest()
-    {}
+    {
+        val value = context.getRequestBody().toBoolean()
+        repo.setAuthAllowGuest(value)
+    }
 }
