e07e78b8df2e6dcbfa79d422b5b8f24970afab2e
Alex <alex.hill@gmail.com>
2020-04-02 13:42:41 +0100

    fix migration

--- migrations/sql/V2020.04.02_1216__FixAdminPermissions.sql
+++ migrations/sql/V2020.04.02_1216__FixAdminPermissions.sql
@@ -1,4 +1,3 @@
-DELETE FROM orderlyweb_user_group_global_permission where user_group = 'Admin';
-
 INSERT INTO "orderlyweb_user_group_global_permission"
-SELECT ID FROM "orderlyweb_user_group_permission" WHERE user_group = 'Admin';
+SELECT ID FROM "orderlyweb_user_group_permission"
+WHERE user_group = 'Admin' and permission = 'documents.manage' or  permission = 'tags.manage'
