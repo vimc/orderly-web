1c42deb4468ac90056af3e234e2e6382743bedf2
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-04-05 16:52:43 +0100

    Added tests

---
+++ src/app/static/src/tests/components/admin/settings.test.js
@@ -0,0 +1,61 @@
+import {shallowMount} from '@vue/test-utils';
+import {mockAxios} from "../../mockAxios";
+import Settings from "../../../js/components/admin/settings.vue";
+import ErrorInfo from "../../../js/components/errorInfo";
+
+describe("settings", () => {
+
+    const url =' http://app/settings/auth-allow-guest/';
+    beforeEach(() => {
+        mockAxios.reset();
+        mockAxios.onGet(url)
+            .reply(200, {"data": true});
+    });
+
+    it('gets allow guest setting and renders value', async () => {
+        const wrapper = shallowMount(Settings);
+        setTimeout(() => {
+            expect(mockAxios.history.get.length).toBe(1);
+            expect(mockAxios.history.get[0].url).toBe(url);
+            expect(wrapper.find("input").attr("checked")).toBe(true);
+            done();
+        });
+    });
+
+    it('sets allow guest setting when value changes', () => {
+        mockAxios.onPost(url)
+            .reply(200);
+        const wrapper = shallowMount(Settings);
+        wrapper.setData({authAllowGuest: false});
+        wrapper.vm.setAuthAllowGuest();
+        setTimeout(() => {
+            expect(mockAxios.history.post.length).toBe(1);
+            expect(mockAxios.history.post[0].url).toBe(url);
+            expect(mockAxios.history.post[0].data).toBe(false);
+            done();
+        });
+    });
+
+    it('shows error from fetching setting', () => {
+        mockAxios.onGet(url)
+            .reply(500, "TEST ERROR");
+        const wrapper = shallowMount(Settings);
+        setTimeout(() => {
+            expect(wrapper.find(ErrorInfo).props().error.response.data).toBe("TEST ERROR");
+            expect(wrapper.find(ErrorInfo).props().defaultMessage).toBe("could not get allow guest user");
+            done();
+        });
+    });
+
+    it('shows error from posting setting', () => {
+        mockAxios.onPost(url)
+            .reply(500, "TEST ERROR");
+        const wrapper = shallowMount(Settings);
+        wrapper.vm.setAuthAllowGuest();
+        setTimeout(() => {
+            expect(wrapper.find(ErrorInfo).props().error.response.data).toBe("TEST ERROR");
+            expect(wrapper.find(ErrorInfo).props().defaultMessage).toBe("could not get allow guest user");
+            done();
+        });
+    });
+});
