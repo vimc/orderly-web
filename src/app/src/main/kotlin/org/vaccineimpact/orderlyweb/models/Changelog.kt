d50ceec2e48c714f8b6d279d83d9bb99265cbbcf
Alex <alex.hill@gmail.com>
2020-04-07 11:05:07 +0100

    add public val to changelog constructor

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/models/Changelog.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/models/Changelog.kt
@@ -3,7 +3,7 @@ package org.vaccineimpact.orderlyweb.models
 import java.beans.ConstructorProperties
 
 data class Changelog
-@ConstructorProperties("reportVersion", "label", "value", "fromFile")
+@ConstructorProperties("reportVersion", "label", "value", "fromFile", "public")
 constructor(val reportVersion: String,
             val label: String,
             val value: String,
