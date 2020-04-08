57eb6a603dc375872e429bd5166a3720e26168ec
Alex <alex.hill@gmail.com>
2020-04-08 16:57:58 +0100

    tests for custom fields

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/database_tests/ReportRepositoryTests/VersionTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/database_tests/ReportRepositoryTests/VersionTests.kt
@@ -11,6 +11,7 @@ import org.vaccineimpact.orderlyweb.db.repositories.ReportRepository
 import org.vaccineimpact.orderlyweb.errors.UnknownObjectError
 import org.vaccineimpact.orderlyweb.test_helpers.CleanDatabaseTests
 import org.vaccineimpact.orderlyweb.test_helpers.insertReport
+import org.vaccineimpact.orderlyweb.test_helpers.insertReportWithCustomFields
 
 class VersionTests: CleanDatabaseTests() {
 
@@ -189,5 +190,32 @@ class VersionTests: CleanDatabaseTests() {
         Assertions.assertThat(results[6].name).isEqualTo("test3")
         Assertions.assertThat(results[6].id).isEqualTo("test3versionunpublished")
         Assertions.assertThat(results[6].latestVersion).isEqualTo("test3versionunpublished")
+    }
+
+    @Test
+    fun `can get all custom fields`()
+    {
+        val sut = createSut()
+        val result = sut.getAllCustomFields()
+        Assertions.assertThat(result.keys).containsExactly("author", "requester")
+        Assertions.assertThat(result["author"]).isEqualTo(null)
+        Assertions.assertThat(result["requester"]).isEqualTo(null)
+    }
+
+    @Test
+    fun `can get custom fields for versions`()
+    {
+        insertReportWithCustomFields("test1", "v1", mapOf("author" to "authorer"))
+        insertReportWithCustomFields("test2", "v2", mapOf())
+        insertReportWithCustomFields("test2", "v3", mapOf("requester" to "requester mcfunderface"))
+
+        val sut = createSut()
+        val result = sut.getCustomFieldsForVersions(listOf("v1", "v2", "v3"))
+
+        Assertions.assertThat(result.keys).containsExactly("v1", "v3")
+        Assertions.assertThat(result["v1"]!!.keys).containsExactly("author")
+        Assertions.assertThat(result["v1"]!!["author"]).isEqualTo("authorer")
+        Assertions.assertThat(result["v3"]!!.keys).containsExactly("requester")
+        Assertions.assertThat(result["v3"]!!["requester"]).isEqualTo("requester mcfunderface")
     }
 }
