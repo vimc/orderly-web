0a67e5c8caa22ce7327b0f3440208863c407f7a1
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-05-04 17:27:03 +0100

    Added trigger to migration

--- migrations/sql/V2020.05.04_1059__OrderlyWebReportVersion.sql
+++ migrations/sql/V2020.05.04_1059__OrderlyWebReportVersion.sql
@@ -8,3 +8,10 @@ PRIMARY KEY("id")
 INSERT INTO "orderlyweb_report_version"
 SELECT id, published
 FROM report_version;
+
+CREATE TRIGGER insert_orderlyweb_report_version
+   AFTER INSERT ON report_version
+BEGIN
+    INSERT OR IGNORE INTO orderlyweb_report_version(id, published)
+    VALUES(NEW.id, false);
+END;
