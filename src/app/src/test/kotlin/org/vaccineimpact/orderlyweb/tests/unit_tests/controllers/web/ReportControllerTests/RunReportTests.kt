59f79d12a200fa418dbd345a7b1579c7c6b04fef
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-05-13 16:20:56 +0100

    Added tests

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/ReportControllerTests/RunReportTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/web/ReportControllerTests/RunReportTests.kt
@@ -15,8 +15,14 @@ class RunReportTests: TeamcityTests()
         val mockContext = mock<ActionContext>()
         val sut = ReportController(mockContext, mock(), mock(), mock())
         val result = sut.getRunReport()
+
         assertThat(result.breadcrumbs.count()).isEqualTo(2)
         assertThat(result.breadcrumbs[1].name).isEqualTo("Run a report")
         assertThat(result.breadcrumbs[1].url).isEqualTo("http://localhost:8888/run-report")
+        assertThat(result.gitBranches).hasSameElementsAs(listOf("master", "dev_branch"))
+        assertThat(result.runReportMetadata.gitSupported).isTrue()
+        assertThat(result.runReportMetadata.instancesSupported).isTrue()
+        assertThat(result.runReportMetadata.instances).hasSameElementsAs(listOf("support", "annex"))
+        assertThat(result.runReportMetadata.changelogTypes).hasSameElementsAs(listOf("internal", "published"))
     }
 }
