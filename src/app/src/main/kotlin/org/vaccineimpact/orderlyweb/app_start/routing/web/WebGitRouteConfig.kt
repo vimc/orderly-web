55340ab7aefdccb007e1dd223c551a5b0345b57c
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-05-14 16:48:28 +0100

    Add route

---
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/app_start/routing/web/WebGitRouteConfig.kt
@@ -0,0 +1,8 @@
+package org.vaccineimpact.orderlyweb.app_start.routing.web
+
+import org.vaccineimpact.orderlyweb.app_start.RouteConfig
+
+object WebGitRouteConfig : RouteConfig
+{
+    private val runReports = setOf("*/reports.run")
+}
