70c40585b4a9c1878e3be711b7c4243b874dc060
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-06-04 10:28:13 +0100

    Add commti select to front end

--- src/app/static/src/js/components/runReport/runReport.vue
+++ src/app/static/src/js/components/runReport/runReport.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
         <form class="mt-3">
-            <div v-if="metadata.git_supported" id="gitBranchFormGroup" class="form-group row">
-                <label for="gitBranch" class="col-sm-2 col-form-label text-right">Git branch</label>
-                <div class="col-sm-6">
-                    <select class="form-control" id="gitBranch" v-model="selectedBranch" @change="changedBranch">
-                        <option disabled selected value=""> -- Select a branch -- </option>
-                        <option v-for="branch in gitBranches" :value="branch">{{branch}}</option>
-                    </select>
+            <div v-if="metadata.git_supported">
+                <div id="git-branch-form-group" class="form-group row">
+                    <label for="git-branch" class="col-sm-2 col-form-label text-right">Git branch</label>
+                    <div class="col-sm-6">
+                        <select class="form-control" id="git-branch" v-model="selectedBranch" @change="changedBranch">
+                            <option disabled selected value=""> -- Select a branch -- </option>
+                            <option v-for="branch in gitBranches" :value="branch">{{branch}}</option>
+                        </select>
+                    </div>
+                </div>
+                <div v-if="showCommits" id="git-commit-form-group" class="form-group row">
+                    <label for="git-commit" class="col-sm-2 col-form-label text-right">Git commit</label>
+                    <div class="col-sm-6">
+                        <select class="form-control" id="git-commit" v-model="selectedCommit.id" @change="changedCommit">
+                            <option v-for="(commit, idx) in gitCommits" :value="commit.id">
+                                {{commit.id}}
+                            </option>
+                        </select>
+                    </div>
                 </div>
             </div>
          </form>
@@ -15,24 +27,34 @@
 </template>
 
 <script>
+    import {api} from "../../utils/api";
     export default {
         name: "runReport",
         props: ['metadata', 'gitBranches'],
         data: () => {
             return {
                 gitCommits: [],
-                selectedBranch: ""
+                selectedBranch: "",
+                selectedCommit: ""
             }
         },
         computed: {
             showCommits: function() {
-                return this.gitCommits && this.gitCommits.length > 0;
+                return this.gitCommits && this.gitCommits.length;
             },
            changedBranch: function() {
-                //TODO:
-                if (this.selecteBranch) {
-                    alert("selected branch " + this.selectedBranch);
+                if (this.selectedBranch) {
+                    api.get(`/git/branch/${this.selectedBranch}/commits/`)
+                        .then(({data}) => {
+                            this.gitCommits = data.data;
+                            if (this.gitCommits.length) {
+                                this.selectedCommit = this.gitCommits[0];
+                            }
+                        })
                 }
+            },
+            changedCommit: function() {
+                //TODO
             }
         }
     }
