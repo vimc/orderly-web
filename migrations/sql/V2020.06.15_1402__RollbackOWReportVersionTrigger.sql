3cef5966259ce6421ef9fb8489735aca854fc714
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-06-15 15:30:23 +0100

    Use local branch

--- migrations/sql/V2020.06.15_1402__RollbackOWReportVersionTrigger.sql
+++ migrations/sql/V2020.06.15_1402__RollbackOWReportVersionTrigger.sql
@@ -1,9 +1,9 @@
-DROP TRIGGER insert_orderlyweb_report_version
-DROP VIEW orderlyweb_report_version_full
+DROP TRIGGER insert_orderlyweb_report_version;
+DROP VIEW orderlyweb_report_version_full;
 
 CREATE VIEW orderlyweb_report_version_full AS
 SELECT rv.id, report, date, displayname, description, connection, elapsed, git_sha, git_branch, git_clean, requester, author,
     COALESCE(ow_rv.published, FALSE) as published
 FROM report_version rv
 LEFT OUTER JOIN orderlyweb_report_version ow_rv
-ON rv.id = ow_rv.id
+ON rv.id = ow_rv.id;
