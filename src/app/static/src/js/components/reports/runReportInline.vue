381d07554e9d50a119c9e4eed8d03e372f3bf816
Alex <alex.hill@gmail.com>
2020-07-16 11:27:41 +0100

    use @ syntax

--- src/app/static/src/js/components/reports/runReportInline.vue
+++ src/app/static/src/js/components/reports/runReportInline.vue
@@ -9,19 +9,19 @@
                     <div class="mb-2 font-weight-bold">Confirm run report</div>
                     <div class="mb-2">Are you sure you want to run this report?</div>
                     <div class="modal-buttons">
-                        <button v-on:click="run" id="confirm-run-btn" class="btn submit mr-3">Yes</button>
-                        <button v-on:click="cancelRun" id="cancel-run-btn" class="btn btn-default">No</button>
+                        <button @click="run" id="confirm-run-btn" class="btn submit mr-3">Yes</button>
+                        <button @click="cancelRun" id="cancel-run-btn" class="btn btn-default">No</button>
                     </div>
                 </div>
             </div>
         </div>
-        <button v-on:click="confirmRun" class="btn mt-2" type="submit">Run report</button>
+        <button @click="confirmRun" class="btn mt-2" type="submit">Run report</button>
         <div id="run-report-status" v-if="runningStatus" class="text-secondary mt-2">
             Running status: {{runningStatus}}
             <div v-if="runHasCompleted && newVersionFromRun" id="run-report-new-version">
                 New version: <a v-bind:href="newVersionHref">{{newVersionDisplayName}}</a>
             </div>
-            <button v-on:click="dismissRunStatus" id="run-report-dismiss" class="btn btn-link">Dismiss</button>
+            <button @click="dismissRunStatus" id="run-report-dismiss" class="btn btn-link">Dismiss</button>
         </div>
     </div>
 </template>
