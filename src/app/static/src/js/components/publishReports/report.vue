d35e57937717d1374f7724877d1bbb65cacf8a68
Alex <alex.hill@gmail.com>
2020-08-10 15:40:59 +0100

    remove console log

--- src/app/static/src/js/components/publishReports/report.vue
+++ src/app/static/src/js/components/publishReports/report.vue
@@ -52,7 +52,6 @@ export default {
         },
         handleDraftSelect(value) {
             if (!value.value) {
-                console.log("hi")
                 // don't want the parent checkbox selected if any child is not
                 this.selected = false
             }
