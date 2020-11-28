69db1e6248f929a3124828dc63cb8cc4b2046200
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2020-11-28 19:05:30 +0000

    Cleaner approach to making test idempotent

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/BundleTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/BundleTests.kt
@@ -8,14 +8,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.vaccineimpact.orderlyweb.ContentTypes
 import org.vaccineimpact.orderlyweb.db.AppConfig
-import org.vaccineimpact.orderlyweb.db.JooqContext
-import org.vaccineimpact.orderlyweb.db.Tables
 import org.vaccineimpact.orderlyweb.db.getResource
 import org.vaccineimpact.orderlyweb.security.WebTokenHelper
 import org.vaccineimpact.orderlyweb.tests.integration_tests.helpers.fakeGlobalReportReviewer
 import org.vaccineimpact.orderlyweb.tests.integration_tests.tests.IntegrationTest
 import java.io.ByteArrayInputStream
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.zip.ZipInputStream
 
 class BundleTests : IntegrationTest() {
@@ -40,12 +40,9 @@ class BundleTests : IntegrationTest() {
         val reportVersion = "20201126-153124-9a32811a"
         val orderlyRoot = AppConfig()["orderly.root"]
 
-        File("${orderlyRoot}archive/$reportName/$reportVersion/").deleteRecursively()
-        JooqContext("${orderlyRoot}orderly.sqlite", false).use {
-            it.dsl.deleteFrom(Tables.REPORT_VERSION)
-                .where(Tables.REPORT_VERSION.ID.eq(reportVersion))
-                .execute()
-        }
+        // Backup orderly repository state
+        val dbPath = Paths.get("${orderlyRoot}orderly.sqlite")
+        val dbContent = Files.readAllBytes(dbPath)
 
         val token = WebTokenHelper.instance.issuer.generateBearerToken(fakeGlobalReportReviewer())
         val body = getResource("$reportVersion.zip").readBytes()
@@ -56,6 +53,10 @@ class BundleTests : IntegrationTest() {
                 .build()
         val response = OkHttpClient().newCall(request).execute()
         assertThat(response.code).isEqualTo(200)
+
+        // Restore orderly repository state
+        Files.write(dbPath, dbContent)
+        File("${orderlyRoot}archive/$reportName/$reportVersion/").deleteRecursively()
     }
 
     private fun getZipEntries(response: khttp.responses.Response): MutableList<String> {
