9ed93e392d47bad7ff3279ac45493cfb0d16d208
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-01-14 16:45:17 +0000

    run built cli image

--- buildkite/build-cli.sh
+++ buildkite/build-cli.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-HERE=$(dirname $0)
-. $HERE/common
+here=$(dirname $0)
+. $here/common
 
 # This is the path for teamcity agents. If running locally, pass in your own docker config location
 # i.e. /home/{user}/.docker/config.json
@@ -19,16 +19,14 @@ docker build --tag orderly-web-cli-build \
     -f cli.Dockerfile \
     .
 
-# TODO test?
-
 # Migrate the test db
-#./scripts/migrate-test.sh
+./scripts/migrate-test.sh
 
 # Run the created image
-#docker run --rm \
-#    -v /var/run/docker.sock:/var/run/docker.sock \
-#    -v $docker_auth_path:/root/.docker/config.json \
-#    -v $PWD/demo:/api/src/userCLI/demo \
-#    --network=host \
-#    orderly-web-cli-build
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $BUILDKITE_DOCKER_AUTH_PATH:/root/.docker/config.json \
+    -v $PWD/demo:/api/src/userCLI/demo \
+    --network=host \
+    orderly-web-cli-build
 
