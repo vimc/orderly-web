b30794bd0e125a84afa76e332261d4895c4aea74
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-01-28 09:05:05 +0000

    Run report page: user can select report to run

---
+++ src/app/static/src/tests/components/runReport/reportList.test.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import {mount} from "@vue/test-utils";
+import ReportList from "../../../js/components/runReport/reportList.vue";
+
+function getWrapper() {
+    return mount(ReportList, {
+        propsData: {
+            reports: [
+                {name: "report2", date: null},
+                {name: "report1", date: new Date().toISOString()}
+            ]
+        }
+    });
+}
+
+describe("reportList", () => {
+
+    it("renders typeahead correctly and fires event on selection", (done) => {
+        const wrapper = getWrapper();
+
+        const reportSuggestions = wrapper.findAll("a div.sr-only");
+        expect(reportSuggestions.length).toBe(2);
+        expect(reportSuggestions.at(0).text()).toBe("report1");
+        expect(reportSuggestions.at(1).text()).toBe("report2");
+        reportSuggestions.at(1).trigger("click");
+        expect(wrapper.emitted()["update:report"].length).toBe(1);
+        expect(wrapper.emitted()["update:report"][0]).toEqual(["report2"]);
+
+        done();
+    });
+
+    it("typeahead filters list correctly", async (done) => {
+        const wrapper = getWrapper();
+
+        wrapper.find("input").setValue("rt2");
+
+        await Vue.nextTick();
+
+        const reportSuggestions = wrapper.findAll("a div.sr-only");
+        expect(reportSuggestions.length).toBe(1);
+        expect(reportSuggestions.at(0).text()).toBe("report2");
+
+        done();
+    });
+
+});
--- src/app/static/src/tests/components/runReport/runReport.test.js
+++ src/app/static/src/tests/components/runReport/runReport.test.js
@@ -1,22 +1,43 @@
 import Vue from "vue";
 import {shallowMount, mount} from "@vue/test-utils";
 import RunReport from "../../../js/components/runReport/runReport.vue";
+import ReportList from "../../../js/components/runReport/reportList.vue";
 import ErrorInfo from "../../../js/components/errorInfo";
 import {mockAxios} from "../../mockAxios";
 
-describe("reportTags", () => {
+describe("runReport", () => {
     beforeEach(() => {
         mockAxios.reset();
         mockAxios.onGet('http://app/git/branch/master/commits/')
-            .reply(200, {"data": mockCommits});
+            .reply(200, {"data": gitCommits});
     });
 
-    const mockCommits = [
+    const gitCommits = [
         {id: "abcdef", date_time: "Mon Jun 08, 12:01"},
         {id: "abc123", date_time: "Tue Jun 09, 13:11"}
     ];
 
     const gitBranches = ["master", "dev"];
+
+    const reports = [
+        {name: "report1", date: new Date().toISOString()},
+        {name: "report2", date: null}
+    ];
+
+    const getWrapper = (reports) => {
+        mockAxios.onGet('http://app/reports/runnable/?branch=master&commit=abcdef')
+            .reply(200, {"data": reports});
+
+        return mount(RunReport, {
+            propsData: {
+                metadata: {
+                    git_supported: true,
+                    instances_supported: false
+                },
+                gitBranches
+            }
+        });
+    }
 
     it("renders git branch drop down and fetches commits if git supported", (done) => {
 
@@ -56,14 +77,16 @@ describe("reportTags", () => {
         });
 
         await Vue.nextTick();
-        expect(mockAxios.history.get.length).toBe(0);
+        expect(mockAxios.history.get.length).toBe(1);
         expect(wrapper.find("#git-branch-form-group").exists()).toBe(false);
         expect(wrapper.find("#git-commit-form-group").exists()).toBe(false);
     });
 
     it("calls api to get commits when branch changes and updates commits drop down", (done) => {
         mockAxios.onGet('http://app/git/branch/dev/commits/')
-            .reply(200, {"data": mockCommits});
+            .reply(200, {"data": gitCommits});
+        mockAxios.onGet('http://app/reports/runnable/?branch=dev&commit=abcdef')
+            .reply(200, {"data": []});
 
         const wrapper = mount(RunReport, {
             propsData: {
@@ -90,7 +113,7 @@ describe("reportTags", () => {
         })
     });
 
-    it("show error message if error getting git commits", () => {
+    it("show error message if error getting git commits", (done) => {
         mockAxios.onGet('http://app/git/branch/master/commits/')
             .reply(500, "TEST ERROR");
 
@@ -108,6 +131,31 @@ describe("reportTags", () => {
         })
     });
 
+    it("updates reports dropdown by calling api when commit changes", (done) => {
+        const wrapper = getWrapper(reports);
+
+        setTimeout(() => {
+            expect(mockAxios.history.get.length).toBe(2);
+            expect(wrapper.find(ErrorInfo).props("apiError")).toBe("");
+            expect(wrapper.find(ErrorInfo).props("defaultMessage")).toBe("");
+            expect(wrapper.find(ReportList).props("reports")).toEqual(expect.arrayContaining(reports));
+            done();
+        });
+    });
+
+    it("displays report list in order and allows selection and reset", (done) => {
+        const wrapper = getWrapper(reports);
+
+        setTimeout(async () => {
+            wrapper.find(ReportList).find("a:last-of-type").trigger("click");
+            expect(wrapper.vm.$data["selectedReport"]).toBe("report2");
+            await Vue.nextTick();
+            wrapper.find(ReportList).find("button").trigger("click");
+            expect(wrapper.vm.$data["selectedReport"]).toBe("");
+            done();
+        });
+    });
+
     it("shows instances if instances supported", () => {
         const wrapper = shallowMount(RunReport, {
             propsData: {
@@ -121,6 +169,11 @@ describe("reportTags", () => {
                     }
                 },
                 gitBranches
+            },
+            data() {
+                return {
+                    selectedReport: "report1"
+                }
             }
         });
 
