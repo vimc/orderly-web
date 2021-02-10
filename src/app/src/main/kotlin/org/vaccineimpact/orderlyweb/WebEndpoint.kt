d3e1eb288c619ba7675dffcb4aee4dd28ea60fbf
Lekan Anifowoshe <anney.lake@gmail.com>
2021-02-10 18:56:54 +0000

    fix lint

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/WebEndpoint.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/WebEndpoint.kt
@@ -49,7 +49,7 @@ data class WebEndpoint(
 
     private fun addSecurityFilter(url: String)
     {
-        //If Montagu Auth and if guest users are not allowed, OrderlyWeb auth should be fully synchronised
+        // If Montagu Auth and if guest users are not allowed, OrderlyWeb auth should be fully synchronised
         // with the external login provider, and login page should not be seen
         val synchronisedAuth = authenticationConfig.getConfiguredProvider() == AuthenticationProvider.Montagu
 
