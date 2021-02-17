156d16d86b6a178ecb9537f2c5b38ba887112c6e
Lekan Anifowoshe <anney.lake@gmail.com>
2021-02-17 09:17:00 +0000

    place elements on the screen

--- src/app/static/src/js/components/reportLog/reportLog.vue
+++ src/app/static/src/js/components/reportLog/reportLog.vue
@@ -1,12 +1,40 @@
 <template>
-<div>
-    <div v-if="showReports" id="report-form-group" class="form-group row">
-        <label for="report" class="col-sm-2 col-form-label text-right">Show logs for</label>
-        <div class="col-sm-6">
-            <report-list id="report" :reports="reports" :report.sync="selectedReport"/>
+    <div>
+        <div v-if="showReports" id="report-form-group" class="form-group row">
+            <label for="report" class="col-sm-2 col-form-label text-right">Show logs for</label>
+            <div class="col-sm-8">
+                <report-list id="report" :reports="report" :report.sync="selectedReport"/>
+            </div>
+        </div>
+        <div v-if="showReportLog" id="report-log">
+            <div class="form-group row">
+                <div class="col-sm-2 text-right">Github branch:</div>
+                <div class="col-auto font-weight-bold">{{ reportLog.gitBranch }}</div>
+                <div class="text-right">Github commit:</div>
+                <div class="col-auto font-weight-bold">{{ reportLog.gitCommit }}</div>
+                <div>Instance(s):</div>
+                <div class="col-auto font-weight-bold">{{ reportLog.instances }}</div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-sm-2 text-right">Status:</div>
+                <div class="col-auto font-weight-bold">{{ reportLog.status }}</div>
+                <div class="text-right">Report version:</div>
+                <div class="col-auto font-weight-bold">{{reportLog.reportVersion}}</div>
+                <div class="text-right">Parameters:</div>
+                <div class="col-auto font-weight-bold">{{ reportLog.params }}</div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-sm-2 text-right"></div>
+                <div class="col-sm-8 vh-100">
+                <textarea class="form-control bg-white" readonly rows="10">
+                {{ reportLog.log }}
+               </textarea>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -17,6 +45,7 @@
 
     interface Computed {
         showReports: boolean
+        showReportLog: boolean
     }
 
     interface Methods {
@@ -29,19 +58,20 @@
         error: string,
         defaultMessage: string,
         reportId: string
+        report,
     }
 
     const initialReportLog = {
-        email: "",
-        date: "",
-        report: "",
-        instances: "",
-        params: "",
-        gitBranch: "",
-        gitCommit: "",
-        status: "",
-        log: "",
-        reportVersion: ""
+        email: "test",
+        date: "test",
+        report: "test",
+        instances: "test",
+        params: "test",
+        gitBranch: "test",
+        gitCommit: "test",
+        status: "test",
+        log: "test",
+        reportVersion: "test"
     }
 
     export default Vue.extend<Data, Methods, Computed, unknown>({
@@ -51,6 +81,7 @@
         },
         data(): Data {
             return {
+                report: [],
                 reportId: "",
                 selectedReport: "",
                 reportLog: initialReportLog,
@@ -60,6 +91,9 @@
         },
         computed: {
             showReports: function () {
+                return true
+            },
+            showReportLog: function () {
                 return true
             }
         },
