e14eee7a5d0f6484bab0fe8735d8dc20c79dafeb
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-03-10 16:02:23 +0000

    Fix docker run in make-db

--- buildkite/make-db.sh
+++ buildkite/make-db.sh
@@ -1,3 +1,5 @@
+set -ex
+
 ORDERLY_IMAGE=$ORG/orderly:master
 MIGRATE_IMAGE=$ORG/orderlyweb-migrate:$GIT_ID
 
@@ -10,7 +12,7 @@ docker run --rm \
     -u $UID \
     -v $PWD:/orderly \
     -w "/orderly" \
-    --env "HOME=/tmp"
+    --env "HOME=/tmp" \
     $ORDERLY_IMAGE \
     "."
 
