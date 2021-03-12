a610beb9c3e07e842bbedc277553d4e3ec179c27
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-03-12 15:59:40 +0000

    implemented Mark's comments

--- src/app/static/src/js/components/runReport/runReportTabs.vue
+++ src/app/static/src/js/components/runReport/runReportTabs.vue
@@ -11,10 +11,10 @@
                         <div class="d-md-block mt-4 mt-md-0 collapse navbar-collapse" id="sidebar">
                             <ul class="nav flex-column list-unstyled mb-0">
                                 <li class="nav-item">
-                                    <a id="run-link" class="nav-link active" data-toggle="tab" role="tab" @click="switchTab('runReport')">Run a report</a>
+                                    <a id="run-link" class="nav-link active" data-toggle="tab" role="tab" href="#" @click="switchTab('runReport')">Run a report</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a id="logs-link" class="nav-link" data-toggle="tab" role="tab" @click="switchTab('reportLogs')">Report logs</a>
+                                    <a id="logs-link" class="nav-link" data-toggle="tab" role="tab" href="#" @click="switchTab('reportLogs')">Report logs</a>
                                 </li>
                             </ul>
                         </div>
