b642ff85a8e590b4d4a8324c449b915d1fb93232
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-03-16 17:14:35 +0000

    Added tests

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/QueueTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/QueueTests.kt
@@ -1,5 +1,6 @@
 package org.vaccineimpact.orderlyweb.tests.integration_tests.tests.api
 
+import com.fasterxml.jackson.databind.node.ArrayNode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.vaccineimpact.orderlyweb.tests.integration_tests.helpers.fakeGlobalReportReader
@@ -11,12 +12,17 @@ class QueueTests : IntegrationTest()
     @Test
     fun `can get queue status`()
     {
-        val runResponse = apiRequestHelper.post("/reports/minimal/run/",
+        apiRequestHelper.post("/reports/minimal/run/",
                 mapOf("params" to mapOf<String, String>()),
                 userEmail = fakeGlobalReportReviewer())
 
         val response = apiRequestHelper.get("/queue/status", userEmail = fakeGlobalReportReader())
-        assertThat(response.text).isEqualTo("argh")
-        //Actual   :"{"data":{"tasks":[{"name":"minimal","version":null,"key":"lightsome_blackmamba","status":"running"}]},"errors":[],"status":"success"}"
+
+        JSONValidator.validateAgainstSchema(response.text, "QueueStatus")
+        val tasks = JSONValidator.getData(response.text)["tasks"] as ArrayNode
+        assertThat(tasks.count()).isEqualTo(1)
+        assertThat(tasks[0]["name"].textValue()).isEqualTo("minimal")
+        assertThat(tasks[0]["key"].textValue()).isNotEmpty()
+        assertThat(tasks[0]["status"].textValue()).isNotEmpty()
     }
 }
---
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/unit_tests/controllers/api/QueueControllerTests.kt
@@ -0,0 +1,27 @@
+package org.vaccineimpact.orderlyweb.tests.unit_tests.controllers.api
+
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.vaccineimpact.orderlyweb.ActionContext
+import org.vaccineimpact.orderlyweb.OrderlyServerAPI
+import org.vaccineimpact.orderlyweb.OrderlyServerResponse
+import org.vaccineimpact.orderlyweb.controllers.api.QueueController
+
+class QueueControllerTests : ControllerTest()
+{
+    @Test
+    fun `getStatus fetches queue status from orderly server`()
+    {
+        val mockContext = mock<ActionContext>()
+        val mockResponse = OrderlyServerResponse("testResponse", 200)
+        val mockOrderly = mock<OrderlyServerAPI>{
+            on { it.get("/v1/queue/status/", mockContext) } doReturn mockResponse
+        }
+
+        val sut = QueueController(mockContext, mock(), mockOrderly)
+        val response = sut.getStatus()
+        assertThat(response).isEqualTo("testResponse")
+    }
+}
