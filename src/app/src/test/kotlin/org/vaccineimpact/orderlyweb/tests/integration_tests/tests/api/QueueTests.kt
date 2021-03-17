c70b923736036f85578b89f0fe7a8bf0e1c1800b
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-03-17 09:53:19 +0000

    Added integration test for non-auth case

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/QueueTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/api/QueueTests.kt
@@ -3,6 +3,8 @@ package org.vaccineimpact.orderlyweb.tests.integration_tests.tests.api
 import com.fasterxml.jackson.databind.node.ArrayNode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.vaccineimpact.orderlyweb.ContentTypes
+import org.vaccineimpact.orderlyweb.test_helpers.http.HttpClient
 import org.vaccineimpact.orderlyweb.tests.integration_tests.helpers.fakeGlobalReportReader
 import org.vaccineimpact.orderlyweb.tests.integration_tests.helpers.fakeGlobalReportReviewer
 import org.vaccineimpact.orderlyweb.tests.integration_tests.tests.IntegrationTest
@@ -18,11 +20,20 @@ class QueueTests : IntegrationTest()
 
         val response = apiRequestHelper.get("/queue/status", userEmail = fakeGlobalReportReader())
 
+        assertSuccessful(response)
         JSONValidator.validateAgainstSchema(response.text, "QueueStatus")
         val tasks = JSONValidator.getData(response.text)["tasks"] as ArrayNode
         assertThat(tasks.count()).isEqualTo(1)
         assertThat(tasks[0]["name"].textValue()).isEqualTo("minimal")
         assertThat(tasks[0]["key"].textValue()).isNotEmpty()
         assertThat(tasks[0]["status"].textValue()).isNotEmpty()
+    }
+
+    @Test
+    fun `cannot get queue status if not authenticated`()
+    {
+        val headers = mapOf("Accept" to ContentTypes.json)
+        val response = HttpClient.get(apiRequestHelper.baseUrl + "/reports/minimal/run/", headers)
+        assertThat(response.statusCode).isEqualTo(401)
     }
 }
