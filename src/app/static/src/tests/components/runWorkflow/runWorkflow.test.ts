625314931268e6b3a91374403e38a7f1cfac9cdd
Lekan Anifowoshe <anney.lake@gmail.com>
2021-05-14 13:29:25 +0100

    remove redundant tests

--- src/app/static/src/tests/components/runWorkflow/runWorkflow.test.ts
+++ src/app/static/src/tests/components/runWorkflow/runWorkflow.test.ts
@@ -1,7 +1,6 @@
 import {mount} from "@vue/test-utils";
 import runWorkflow from '../../../js/components/runWorkflow/runWorkflow.vue'
 import workflowWizard from "../../../js/components/workflowWizard/workflowWizard.vue";
-import runWorkflowCreate from "../../../js/components/runWorkflow/runWorkflowCreate.vue";
 
 describe(`runWorkflow`, () => {
 
@@ -20,31 +19,10 @@ describe(`runWorkflow`, () => {
             }
         })
     }
-    it(`display run workflow tab and can render workflow wizard on click event`, async () => {
-        const wrapper = getWrapper()
-        expect(wrapper.find(runWorkflowCreate).exists()).toBe(true)
-        await wrapper.find("#create-workflow").trigger("click")
-        expect(wrapper.find(workflowWizard).exists()).toBe(true)
-        expect(wrapper.vm.$data.entryStep).toBe("report")
-        expect(wrapper.vm.$data.workflowStarted).toBe(true)
-        expect(wrapper.vm.$data.backButtonVisible).toBe(true)
-    })
 
     it(`does not start workflow wizard when run workflow is rendered`, async () => {
         const wrapper = getWrapper()
         expect(wrapper.find(workflowWizard).exists()).toBe(false)
-    })
-
-    it(`can render workflow wizard metadata`, async () => {
-        const wrapper = getWrapper()
-        await wrapper.setData({runWorkflowMetadata: workflowMetadata})
-        await wrapper.find("#create-workflow").trigger("click")
-        expect(wrapper.find(workflowWizard).exists()).toBe(true)
-        expect(wrapper.find(workflowWizard).props("runWorkflowMetadata")).toMatchObject(workflowMetadata)
-        expect(wrapper.find(workflowWizard).props("runWorkflowMetadata").placeholder).toBe("Placeholder works")
-        expect(wrapper.vm.$data.entryStep).toBe("report")
-        expect(wrapper.vm.$data.workflowStarted).toBe(true)
-        expect(wrapper.vm.$data.backButtonVisible).toBe(true)
     })
 
     it(`can start and cancel workflow wizard correctly when starting a workflow wizard from re-run`, async () => {
