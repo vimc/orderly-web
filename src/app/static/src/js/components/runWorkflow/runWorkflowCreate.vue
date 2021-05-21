a423ba3ac44821a26c51123c4c175635c28711da
Lekan Anifowoshe <anney.lake@gmail.com>
2021-05-21 10:13:38 +0100

    add scss

--- src/app/static/src/js/components/runWorkflow/runWorkflowCreate.vue
+++ src/app/static/src/js/components/runWorkflow/runWorkflowCreate.vue
@@ -47,7 +47,6 @@
     import vSelect from 'vue-select'
     import {api} from "../../utils/api";
     import {RunWorkflowMetadata} from "../../utils/types";
-    import "vue-select/dist/vue-select.css";
     import {longTimestamp} from "../../utils/helpers";
     import ErrorInfo from "../errorInfo.vue";
 
