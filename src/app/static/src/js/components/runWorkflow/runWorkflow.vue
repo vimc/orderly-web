8a288611097f5d24e885683b7660ea955af2f50f
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-06-18 17:03:26 +0100

    connected endpoint to frontend

--- src/app/static/src/js/components/runWorkflow/runWorkflow.vue
+++ src/app/static/src/js/components/runWorkflow/runWorkflow.vue
@@ -1,40 +1,45 @@
 <template>
     <div class="container">
-        <run-workflow-create v-if="!workflowStarted"
-                             @rerun="handleRerun"
-                             @create="handleCreate"
-                             @clone="handleClone">
+        <run-workflow-create
+            v-if="!workflowStarted"
+            @rerun="handleRerun"
+            @create="handleCreate"
+            @clone="handleClone"
+        >
         </run-workflow-create>
-        <workflow-wizard v-if="workflowStarted"
-                         :steps="stepComponents"
-                         :submit-label="toggleFinalStepNextTo"
-                         @cancel="handleCancel"
-                         @complete="handleComplete"
-                         :run-workflow-metadata="runWorkflowMetadata">
+        <workflow-wizard
+            v-if="workflowStarted"
+            :steps="stepComponents"
+            :submit-label="toggleFinalStepNextTo"
+            @cancel="handleCancel"
+            @complete="handleComplete"
+            :run-workflow-metadata="runWorkflowMetadata"
+        >
         </workflow-wizard>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from "vue"
-    import workflowWizard from "../workflowWizard/workflowWizard.vue";
-    import {RunWorkflowMetadata, Step} from "../../utils/types"
-    import runWorkflowCreate from "./runWorkflowCreate.vue";
+import Vue from "vue";
+import workflowWizard from "../workflowWizard/workflowWizard.vue";
+import { RunWorkflowMetadata, Step } from "../../utils/types";
+import runWorkflowCreate from "./runWorkflowCreate.vue";
+import { api } from "../../utils/api";
 
-    interface Data {
-        runWorkflowMetadata: RunWorkflowMetadata | null
-        workflowStarted: boolean
-        stepComponents: Step[]
-        toggleFinalStepNextTo: string | null
-    }
+interface Data {
+    runWorkflowMetadata: RunWorkflowMetadata | null;
+    workflowStarted: boolean;
+    stepComponents: Step[];
+    toggleFinalStepNextTo: string | null;
+}
 
-    interface Methods {
-        handleCancel: () => void
-        handleRerun: (data: Event) => void
-        handleCreate: () => void
-        handleClone: (data: Event) => void
-        handleComplete: () => void
-    }
+interface Methods {
+    handleCancel: () => void;
+    handleRerun: (data: Event) => void;
+    handleCreate: () => void;
+    handleClone: (data: Event) => void;
+    handleComplete: () => void;
+}
 
 export default Vue.extend<Data, Methods, unknown, unknown>({
     name: "runWorkflow",
@@ -43,41 +48,53 @@ export default Vue.extend<Data, Methods, unknown, unknown>({
             runWorkflowMetadata: null,
             workflowStarted: false,
             stepComponents: [],
-            toggleFinalStepNextTo: "Run workflow"
-        }
+            toggleFinalStepNextTo: "Run workflow",
+        };
     },
     methods: {
         handleRerun: function (data) {
-            this.runWorkflowMetadata = data
-            this.stepComponents = [{name: "run", component: "runWorkflowRun"}]
-            this.workflowStarted = true
+            this.runWorkflowMetadata = data;
+            this.stepComponents = [
+                { name: "run", component: "runWorkflowRun" },
+            ];
+            this.workflowStarted = true;
         },
         handleCreate: function () {
-            this.runWorkflowMetadata = null
+            this.runWorkflowMetadata = null;
             this.stepComponents = [
-                {name: "report", component: "runWorkflowReport"},
-                {name: "run", component: "runWorkflowRun"},
-            ]
-            this.workflowStarted = true
+                { name: "report", component: "runWorkflowReport" },
+                { name: "run", component: "runWorkflowRun" },
+            ];
+            this.workflowStarted = true;
         },
         handleClone: function (data) {
-            this.runWorkflowMetadata = data
+            this.runWorkflowMetadata = data;
             this.stepComponents = [
-                {name: "report", component: "runWorkflowReport"},
-                {name: "run", component: "runWorkflowRun"},
-            ]
-            this.workflowStarted = true
+                { name: "report", component: "runWorkflowReport" },
+                { name: "run", component: "runWorkflowRun" },
+            ];
+            this.workflowStarted = true;
         },
         handleCancel: function () {
-            this.workflowStarted = false
+            this.workflowStarted = false;
         },
         handleComplete: function () {
-            //handle submitted report here
-        }
+            const data = this.runWorkflowMetadata
+            console.log('data', data)
+            api.post(`/workflow`, data)
+                .then((response) => {
+                    // this.error = null;
+                    console.log('response', response)
+                })
+                .catch((error) => {
+                    // this.error = error;
+                    // this.defaultMessage = `could not add user`;
+                });
+        },
     },
     components: {
         workflowWizard,
-        runWorkflowCreate
-    }
-})
+        runWorkflowCreate,
+    },
+});
 </script>
