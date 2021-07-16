23cd6e82f7d72a9bc049104a275ea3fa8d06e916
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-07-16 16:30:05 +0100

    adjusted test

--- src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/RunWorkflowTests.kt
+++ src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/RunWorkflowTests.kt
@@ -184,7 +184,7 @@ class RunWorkflowTests : SeleniumTest()
         createButton.click()
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("git-branch")))
 
-        assertThat(driver.findElements(By.cssSelector("#git-commit option")).count()).isEqualTo(1)
+        assertThat(driver.findElements(By.cssSelector("#git-commit option")).count()).isEqualTo(2)
 
         val refreshButton = driver.findElement(By.id("git-refresh-btn"))
         refreshButton.click()
