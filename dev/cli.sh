60f813a07216c12175a134cb744a426bfaa63cff
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-08-12 14:59:27 +0100

    updated selenium tests

--- dev/cli.sh
+++ dev/cli.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 image=vimc/orderly-web-user-cli:master
-docker run -v $PWD/src/app/demo:/orderly $image "$@"
+docker run -v $PWD/src/app/git:/orderly $image "$@"
+# docker run -v $PWD/src/app/demo:/orderly $image "$@"
