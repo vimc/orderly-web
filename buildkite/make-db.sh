ba1a808c968c06307bae2fbb0b6c8698cc8bb436
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-08-16 14:20:59 +0100

    Move git fix to make-db so custom config tests pick it up

--- buildkite/make-db.sh
+++ buildkite/make-db.sh
@@ -18,3 +18,6 @@ docker run --rm \
 
 docker run --rm -v ${PWD}/demo:/orderly $MIGRATE_IMAGE
 docker run --rm -v ${PWD}/git:/orderly $MIGRATE_IMAGE
+
+# Fix up git remote
+git --git-dir=$here/../git/.git remote set-url origin /orderly/upstream
