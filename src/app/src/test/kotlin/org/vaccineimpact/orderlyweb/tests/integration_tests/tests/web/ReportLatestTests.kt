6f554e5f569ed97d3eff2b79adf97ae7c4942b10
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-10-05 15:25:00 +0100

    further test changes

--- src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/web/ReportLatestTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/orderlyweb/tests/integration_tests/tests/web/ReportLatestTests.kt
@@ -1,28 +1,30 @@
 package org.vaccineimpact.orderlyweb.tests.integration_tests.tests.web
 
 import org.assertj.core.api.Assertions
-import org.jsoup.Jsoup
+// import org.jsoup.Jsoup
 import org.junit.Test
-import org.vaccineimpact.orderlyweb.ContentTypes
-import org.vaccineimpact.orderlyweb.db.JooqContext
-import org.vaccineimpact.orderlyweb.db.Tables.*
-import org.vaccineimpact.orderlyweb.db.fromJoinPath
-import org.vaccineimpact.orderlyweb.db.joinPath
-import org.vaccineimpact.orderlyweb.models.FilePurpose
+// import org.vaccineimpact.orderlyweb.ContentTypes
+// import org.vaccineimpact.orderlyweb.db.JooqContext
+// import org.vaccineimpact.orderlyweb.db.Tables.*
+// import org.vaccineimpact.orderlyweb.db.fromJoinPath
+// import org.vaccineimpact.orderlyweb.db.joinPath
+// import org.vaccineimpact.orderlyweb.models.FilePurpose
 import org.vaccineimpact.orderlyweb.models.Scope
 import org.vaccineimpact.orderlyweb.models.permissions.ReifiedPermission
 import org.vaccineimpact.orderlyweb.tests.integration_tests.tests.IntegrationTest
 import org.vaccineimpact.orderlyweb.test_helpers.insertReportWithPublishedAndUnpublishedVersions
-import org.vaccineimpact.orderlyweb.db.repositories.OrderlyReportRepository
-import org.vaccineimpact.orderlyweb.db.repositories.ReportRepository
+// import org.vaccineimpact.orderlyweb.db.repositories.OrderlyReportRepository
+// import org.vaccineimpact.orderlyweb.db.repositories.ReportRepository
 import org.vaccineimpact.orderlyweb.tests.integration_tests.WebPermissionChecker
+// import org.vaccineimpact.orderlyweb.controllers.web.ReportController
 
 class ReportLatestTests : IntegrationTest()
 {
-    private fun createSut(isReviewer: Boolean = false): ReportRepository
-    {
-        return OrderlyReportRepository(isReviewer, true, listOf())
-    }
+    // private fun createSut(isReviewer: Boolean = false): ReportController
+    // {
+    //     // return OrderlyReportRepository(isReviewer, true, listOf())
+    //     return ReportController()
+    // }
 
     @Test
     fun `reviewer can get unpublished latest unpublished version`()
@@ -38,7 +40,7 @@ class ReportLatestTests : IntegrationTest()
         val permissions = setOf(ReifiedPermission("reports.review", Scope.Global()))
         val checker = WebPermissionChecker(url, permissions)
         val response = checker.requestWithPermissions(permissions)
-        Assertions.assertThat(response).isEqualTo("expected") //or some more clever test that you're getting the page for expected report version id
+        Assertions.assertThat(response.statusCode).isEqualTo("expected")
 
     }
 }
