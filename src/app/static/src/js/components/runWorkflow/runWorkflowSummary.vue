78471037b64e30c5bc43bfaf5a71e4e1c09e7177
Lekan Anifowoshe <anney.lake@gmail.com>
2021-12-02 16:51:29 +0000

    initial timeline

--- src/app/static/src/js/components/runWorkflow/runWorkflowSummary.vue
+++ src/app/static/src/js/components/runWorkflow/runWorkflowSummary.vue
@@ -1,12 +1,66 @@
 <template>
     <div>
         <h2 id="summary-header">Summary</h2>
-        <span>Your workflow contains {{ reportCount }}:</span>
-        <ul class="styled">
-            <li v-for="report in workflowMetadata.reports">
-                {{ report.name }}
-            </li>
-        </ul>
+        <div id="summary-warning" class="mt-3">
+            <div class="row">
+                <div class="col-auto">icon</div>
+                <div class="col-auto">
+                    <span class="d-inline-block pb-2"> Some reports depend on the latest version of other reports that are not included in your workflow:</span>
+                    <div>
+                        <span class="font-weight-bold">report name</span>
+                        <ul class="styled">
+                            <li>missing dependencies</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="pb-4">
+        <div id="summary-timeline">
+            <div class="row">
+                <div class="col-12">
+                    <div class="summary">
+                        <div v-for="report in workflowMetadata.reports" class="single-timeline-area">
+                            <div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+                            </div>
+                            <div class="d-inline-block"><h5>{{ report.name }}</h5></div>
+                            <div class="row">
+                                <div class="col-12 col-md-6 col-lg-4">
+                                    <div class="single-timeline-content parameters-bg-color d-flex wow fadeInLeft"
+                                         data-wow-delay="0.3s"
+                                         style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                                        <div class="timeline-text">
+                                            <span class="text-muted d-inline-block">Parameters</span>
+                                            <div v-if="report.params.length">
+                                                <p v-for="param in report.params">{{ param }}</p>
+                                                <a href="#" class="pt-2 d-inline-block small">Show default...</a>
+                                            </div>
+                                            <div v-else><p>There are no parameters</p></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="d-inline-block"></span>
+                                <div class="col-12 col-md-6 col-lg-4">
+                                    <div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s"
+                                         style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInLeft;">
+                                        <div class="timeline-text">
+                                            <div class="pb-2">
+                                                <span class="text-muted d-inline-block">Depends on</span>
+                                                <p>Burden-report</p>
+                                            </div>
+                                            <div class="text-danger">
+                                                <span class="d-inline-block">Missing dependencies:</span>
+                                                <p>Touchstone</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
