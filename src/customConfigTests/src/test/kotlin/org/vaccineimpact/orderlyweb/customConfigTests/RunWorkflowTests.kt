37fda79288c9c8c2281014d9c1fc59bc0e13fee5
Lekan Anifowoshe <anney.lake@gmail.com>
2022-01-20 15:43:48 +0000

    fix selenium test

--- src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/RunWorkflowTests.kt
+++ src/customConfigTests/src/test/kotlin/org/vaccineimpact/orderlyweb/customConfigTests/RunWorkflowTests.kt
@@ -408,9 +408,7 @@ class RunWorkflowTests : SeleniumTest()
 
         val defaultParamsOne = driver.findElement(By.id("default-params-0"))
         val defaultParamsCollapsibleOne = defaultParamsOne.findElements(By.id("default-params-collapse"))
-        assertThat(defaultParamsCollapsibleOne.size).isEqualTo(2)
         assertThat(defaultParamsCollapsibleOne[0].text).isEqualTo("nmin: 0.5")
-        assertThat(defaultParamsCollapsibleOne[1].text).isEqualTo("disease: HepB")
     }
 
 }
