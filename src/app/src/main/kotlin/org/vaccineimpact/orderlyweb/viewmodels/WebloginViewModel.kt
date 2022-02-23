f27b2c81f51d8523e8a890d351082f899fb23e1a
Lekan Anifowoshe <anney.lake@gmail.com>
2022-02-23 14:12:55 +0000

    fix lint issue

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/viewmodels/WebloginViewModel.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/viewmodels/WebloginViewModel.kt
@@ -4,4 +4,6 @@ import org.vaccineimpact.orderlyweb.ActionContext
 import org.vaccineimpact.orderlyweb.db.AppConfig
 
 class WebloginViewModel(context: ActionContext, val requestedUrl: String) :
-        AppViewModel by DefaultViewModel(context, IndexViewModel.breadcrumb, Breadcrumb("Login", "${AppConfig()["app.url"]}/weblogin"))
+        AppViewModel by DefaultViewModel(context,
+                IndexViewModel.breadcrumb,
+                Breadcrumb("Login", "${AppConfig()["app.url"]}/weblogin"))
