787deea697cd029242882197a5907d8d0b87f2df
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-03-07 14:56:30 +0000

    fixed floating braces

--- src/app/static/src/js/components/runWorkflow/workflowSummary/runWorkflowSummaryHeader.vue
+++ src/app/static/src/js/components/runWorkflow/workflowSummary/runWorkflowSummaryHeader.vue
@@ -4,37 +4,30 @@
         <div
             id="summary-warning"
             class="row mt-3"
-            v-if="hasMissingDependencies"
-        >
+            v-if="hasMissingDependencies">
             <div class="col-auto">
                 <alert-triangle-icon
                     size="2x"
                     stroke="red"
-                    class="custom-class"
-                />
+                    class="custom-class"/>
             </div>
             <div class="col-auto">
-                <span class="d-inline-block pb-2"
-                    >Some reports depend on the latest version of other reports that are not included in your workflow:</span
-                >
+                <span class="d-inline-block pb-2">
+                    Some reports depend on the latest version of other reports that are not included in your workflow:
+                </span>
                 <div
                     class="missingDependency"
-                    v-for="(
-                        missing_dependencies, report
-                    ) in workflowSummary.missing_dependencies"
-                    :key="report"
-                >
+                    v-for="(missing_dependencies, report) in workflowSummary.missing_dependencies"
+                    :key="report">
                     <span
                         v-if="missing_dependencies.length"
-                        class="font-weight-bold"
-                    >
-                        {{ report }}</span
-                    >
+                        class="font-weight-bold">
+                        {{ report }}
+                    </span>
                     <ul
                         v-for="missing_dependency in missing_dependencies"
                         class="styled"
-                        :key="missing_dependency"
-                    >
+                        :key="missing_dependency">
                         <li>{{ missing_dependency }}</li>
                     </ul>
                 </div>
