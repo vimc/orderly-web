444126262f945d09c20ee75c0fe82c82935c5485
Alex <alex.hill@gmail.com>
2022-03-16 14:38:45 +0000

    fix formatting

--- src/app/static/src/js/components/runWorkflow/runWorkflowParameters.vue
+++ src/app/static/src/js/components/runWorkflow/runWorkflowParameters.vue
@@ -1,67 +1,71 @@
 <template>
     <div v-if="hasParams(report)">
-        <p class="non-default-param"
-            v-for="(param, paramIndex) in report.param_list"
-            :key="paramIndex">{{ param.name }}: {{ param.value }}</p>
-        <p v-if="!report.param_list.length">No non-default parameters</p>
+        <p v-for="(param, paramIndex) in report.param_list"
+           :key="paramIndex"
+           class="non-default-param">{{ param.name }}: {{ param.value }}</p>
+        <p v-if="!report.param_list.length">
+            No non-default parameters
+        </p>
         <div v-if="report.default_param_list.length > 0"
-                :id="`default-params-${index}`">
-            <b-link href="#"
-                    class="show-defaults pt-2 d-inline-block small"
-                    v-b-toggle="`collapseSummary-${index}`">
+             :id="`default-params-${index}`">
+            <b-link v-b-toggle="`collapseSummary-${index}`"
+                    href="#"
+                    class="show-defaults pt-2 d-inline-block small">
                 <span class="when-closed">Show</span>
                 <span class="when-open">Hide</span> defaults...
             </b-link>
             <b-collapse :id="`collapseSummary-${index}`">
-                <p :id="`default-params-collapse-${index}-${paramIndex}`"
-                    v-for="(param, paramIndex) in report.default_param_list"
-                    :key="paramIndex">{{ param.name }}: {{ param.value }}</p>
+                <p v-for="(param, paramIndex) in report.default_param_list"
+                   :id="`default-params-collapse-${index}-${paramIndex}`"
+                   :key="paramIndex">{{ param.name }}: {{ param.value }}</p>
             </b-collapse>
         </div>
     </div>
-    <p v-else>No parameters</p>
+    <p v-else>
+        No parameters
+    </p>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import {WorkflowRunReportStatus, WorkflowReportWithDependencies} from "../../utils/types";
-import {BLink} from "bootstrap-vue/esm/components/link";
-import {BCollapse} from "bootstrap-vue/esm/components/collapse";
-import {VBToggle} from 'bootstrap-vue';
-import {hasParams} from "../../utils/helpers.ts";
+    import Vue from 'vue'
+    import {WorkflowRunReportStatus, WorkflowReportWithDependencies} from "../../utils/types";
+    import {BLink} from "bootstrap-vue/esm/components/link";
+    import {BCollapse} from "bootstrap-vue/esm/components/collapse";
+    import {VBToggle} from 'bootstrap-vue';
+    import {hasParams} from "../../utils/helpers.ts";
 
-interface Props {
-    report: WorkflowRunReportStatus;
-    index: number;
-}
+    interface Props {
+        report: WorkflowRunReportStatus;
+        index: number;
+    }
 
-interface Methods {
-    hasParams: (report: WorkflowReportWithDependencies) => boolean
-}
+    interface Methods {
+        hasParams: (report: WorkflowReportWithDependencies) => boolean
+    }
 
-Vue.directive("b-toggle", VBToggle);
+    Vue.directive("b-toggle", VBToggle);
 
-export default Vue.extend<unknown, Methods, unknown, Props>({
-    name: "runWorkflowParameters",
-    components: {
-        BCollapse,
-        BLink,
-    },
-    props: {
-        report: {
-            type: Object,
-            required: true
+    export default Vue.extend<unknown, Methods, unknown, Props>({
+        name: "RunWorkflowParameters",
+        components: {
+            BCollapse,
+            BLink,
         },
-        index: {
-            type: Number,
-            required: true
+        props: {
+            report: {
+                type: Object,
+                required: true
+            },
+            index: {
+                type: Number,
+                required: true
+            },
         },
-    },
-    methods: {
-        hasParams(report) {
-            return hasParams(report)
+        methods: {
+            hasParams(report) {
+                return hasParams(report)
+            },
         },
-    },
-    
-})
+
+    })
 </script>
--- src/app/static/src/js/components/runWorkflow/runWorkflowTable.vue
+++ src/app/static/src/js/components/runWorkflow/runWorkflowTable.vue
@@ -1,10 +1,18 @@
 <template>
     <table class="table-bordered">
         <tr>
-            <th class="p-2">Report</th>
-            <th v-if="anyParams" class="p-2">Parameters</th>
-            <th class="p-2">Status</th>
-            <th class="p-2">Logs</th>
+            <th class="p-2">
+                Report
+            </th>
+            <th v-if="anyParams" class="p-2">
+                Parameters
+            </th>
+            <th class="p-2">
+                Status
+            </th>
+            <th class="p-2">
+                Logs
+            </th>
         </tr>
         <tr v-for="(report, index) in workflowRunStatus.reports" :key="report.key">
             <td v-if="report.status === 'success'" class="p-2">
@@ -12,19 +20,24 @@
                     {{ report.name }}
                 </a>
             </td>
-            <td v-else class="p-2">{{ report.name }}</td>
+            <td v-else class="p-2">
+                {{ report.name }}
+            </td>
             <td v-if="anyParams" class="p-2">
-                <run-workflow-parameters :report="workflowSummary.reports[index]" :index="index"></run-workflow-parameters>
+                <run-workflow-parameters :report="workflowSummary.reports[index]"
+                                         :index="index"></run-workflow-parameters>
             </td>
             <td :class="statusColour(report.status)" class="p-2">
                 {{ interpretStatus(report.status) }}
             </td>
-            <td v-if="report.date" class="p-2">{{ formatDate(report.date) }}</td>
+            <td v-if="report.date" class="p-2">
+                {{ formatDate(report.date) }}
+            </td>
             <td class="p-2">
                 <a v-if="viewLogLinkVisible(report.status)"
-                    class="report-log-link"
-                    href="#"
-                    @click="$emit('show-report-log', report.key)">
+                   class="report-log-link"
+                   href="#"
+                   @click="$emit('show-report-log', report.key)">
                     View log
                 </a>
             </td>
@@ -33,74 +46,74 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import runWorkflowParameters from "./runWorkflowParameters.vue"
-import {WorkflowRunStatus, WorkflowSummaryResponse} from "../../utils/types";
-import {buildFullUrl} from "../../utils/api";
-import {formatDate, failStates, notStartedStates, interpretStatus, hasParams} from "../../utils/helpers.ts";
+    import Vue from 'vue'
+    import runWorkflowParameters from "./runWorkflowParameters.vue"
+    import {WorkflowRunStatus, WorkflowSummaryResponse} from "../../utils/types";
+    import {buildFullUrl} from "../../utils/api";
+    import {formatDate, failStates, notStartedStates, interpretStatus, hasParams} from "../../utils/helpers.ts";
 
-interface Props {
-    workflowRunStatus: WorkflowRunStatus
-    workflowSummary: WorkflowSummaryResponse | null
-}
+    interface Props {
+        workflowRunStatus: WorkflowRunStatus
+        workflowSummary: WorkflowSummaryResponse | null
+    }
 
-interface Methods {
-    reportVersionHref: (name: string, version: string) => string;
-    statusColour: (status: string) => string;
-    viewLogLinkVisible: (status: string) => boolean;
-    interpretStatus: (status: string) => string;
-    formatDate: (date: string) => string;
-}
+    interface Methods {
+        reportVersionHref: (name: string, version: string) => string;
+        statusColour: (status: string) => string;
+        viewLogLinkVisible: (status: string) => boolean;
+        interpretStatus: (status: string) => string;
+        formatDate: (date: string) => string;
+    }
 
-interface Computed {
-    anyParams: boolean
-}
+    interface Computed {
+        anyParams: boolean
+    }
 
 
-export default Vue.extend<unknown, Methods, Computed, Props>({
-    name: "runWorkflowTable",
-    components: {
-        runWorkflowParameters
-    },
-    props: {
-        workflowRunStatus: {
-            type: Object,
-            required: true
+    export default Vue.extend<unknown, Methods, Computed, Props>({
+        name: "RunWorkflowTable",
+        components: {
+            runWorkflowParameters
         },
-        workflowSummary: {
-            type: Object,
-            default: null
-        }
-    },
-    computed: {
-        anyParams() {
-            return !!this.workflowSummary?.reports.some(report => hasParams(report))
-        }
-    },
-    methods: {
-        reportVersionHref(name, version) {
-            const url = `/report/${name}/${version}/`;
-            return buildFullUrl(url);
-        },
-        statusColour(status) {
-            if (["queued", "running"].includes(status)) {
-                return "text-secondary";
-            } else if (failStates.includes(status)) {
-                return "text-danger";
-            } else {
-                return "";
+        props: {
+            workflowRunStatus: {
+                type: Object,
+                required: true
+            },
+            workflowSummary: {
+                type: Object,
+                default: null
             }
         },
-        viewLogLinkVisible(status) {
-            return !notStartedStates.includes(status);
+        computed: {
+            anyParams() {
+                return !!this.workflowSummary?.reports.some(report => hasParams(report))
+            }
         },
-        interpretStatus(status){
-            return interpretStatus(status)
+        methods: {
+            reportVersionHref(name, version) {
+                const url = `/report/${name}/${version}/`;
+                return buildFullUrl(url);
+            },
+            statusColour(status) {
+                if (["queued", "running"].includes(status)) {
+                    return "text-secondary";
+                } else if (failStates.includes(status)) {
+                    return "text-danger";
+                } else {
+                    return "";
+                }
+            },
+            viewLogLinkVisible(status) {
+                return !notStartedStates.includes(status);
+            },
+            interpretStatus(status) {
+                return interpretStatus(status)
+            },
+            formatDate(date) {
+                return formatDate(date)
+            }
         },
-        formatDate(date){
-            return formatDate(date)
-        },
-    },
-    
-})
+
+    })
 </script>
