5d5cff06d71b3678da1504fd78f7898850a3f3be
Alex <alex.hill@gmail.com>
2022-03-16 15:40:29 +0000

    add component

---
+++ src/app/static/src/js/components/vuex/runReport/gitSelections.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-if="metadata && metadata.git_supported" id="git-branch-form-group" class="form-group row">
+            <label for="git-branch" class="col-sm-2 col-form-label text-right">Git branch</label>
+            <div class="col-sm-6">
+                <select id="git-branch" class="form-control">
+                    <option v-for="branch in gitBranches" :key="branch" :value="branch">
+                        {{ branch }}
+                    </option>
+                </select>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import {mapState} from "vuex";
+    import {RunReportRootState} from "../../../store/runReport/store";
+    import {RunReportMetadataDependency} from "../../../utils/types";
+
+    interface Computed {
+        metadata: RunReportMetadataDependency
+        gitBranches: string[]
+    }
+
+    export default Vue.extend<Record<string, never>, Record<string, never>, Computed, Record<string, never>>({
+        name: "GitSelections",
+        computed: {
+            ...mapState({
+                metadata: (state: RunReportRootState) => state.git.metadata,
+                gitBranches: (state: RunReportRootState) => state.git.git_branches
+            })
+        }
+    });
+</script>
