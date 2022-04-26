3b26e794c084b9f2d54982d759b1b8a52c6b756a
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-04-26 14:46:44 +0100

    added additional unit tests

--- src/app/static/src/tests/components/vuex/runReport/gitSelections.test.ts
+++ src/app/static/src/tests/components/vuex/runReport/gitSelections.test.ts
@@ -1,25 +1,41 @@
 import { shallowMount } from "@vue/test-utils";
 import GitSelections from "../../../../js/components/vuex/runReport/gitSelections.vue";
 import Vuex from "vuex";
+import {ActionContext, ActionTree} from "vuex";
 import { mockGitState, mockRunReportRootState, RecursivePartial } from "../../../mocks";
 import { GitState } from "../../../../js/store/git/git";
+import {GitAction} from "../../../../js/store/git/actions";
 import { namespace } from "../../../../js/store/runReport/store";
+// import {RunnerRootState} from "../../../utils/types";
 
 describe("GitSelections", () => {
 
-    const createStore = (gitState: RecursivePartial<GitState>) => {
+    const mockActions = {
+        fetchMetadata: jest.fn(),
+        selectBranch: jest.fn(),
+        selectCommit: jest.fn()
+    };
+
+    // const createStore = (gitState: RecursivePartial<GitState>, actions: GitAction & ActionTree<GitState, RunnerRootState> = mockActions) => {
+        const createStore = (gitState: RecursivePartial<GitState>) => {
         return new Vuex.Store({
-            state: mockRunReportRootState({
-                git: mockGitState(gitState)
-            }),
+            // state: mockRunReportRootState({
+            //     git: mockGitState(gitState)
+            // }),
             modules: {
                 [namespace.git]: {
                     namespaced: true,
-                    state: mockGitState(gitState)
+                    state: mockGitState(gitState),
+                    // actions
                 }
             }
         })
     }
+
+    const commits = [
+        {id: "commit1", date_time: "time1", age: 1},
+        {id: "commit2", date_time: "time2", age: 2}
+    ]
 
     it("renders git branch drop down if git supported", () => {
 
@@ -53,5 +69,51 @@ describe("GitSelections", () => {
         })
 
         expect(wrapper.find("#git-branch-form-group").exists()).toBe(false);
+    });
+
+    it("renders git commit drop down if has commits", () => {
+
+        const wrapper = shallowMount(GitSelections, {
+            store: createStore({
+                commits
+            })
+        })
+
+        expect(wrapper.find("#git-commit-form-group").exists()).toBe(true);
+        const options = wrapper.findAll("#git-commit-form-group select option");
+        expect(options.length).toBe(2);
+        expect(options.at(0).text()).toBe("commit1 (time1)");
+        expect(options.at(0).attributes().value).toBe("commit1");
+        expect(options.at(1).text()).toBe("commit2 (time2)");
+        expect(options.at(1).attributes().value).toBe("commit2");
+    });
+
+    it("does not render git commit drop down if has no commits", () => {
+
+        const wrapper = shallowMount(GitSelections, {
+            store: createStore({})
+        })
+
+        expect(wrapper.find("#git-commit-form-group").exists()).toBe(false);
+    });
+
+    it("renders git branch drop down if git supported", () => {
+
+        const wrapper = shallowMount(GitSelections, {
+            store: createStore({
+                metadata: {
+                    git_supported: true
+                },
+                branches: ["master", "dev"]
+            })
+        })
+
+        expect(wrapper.find("#git-branch-form-group").exists()).toBe(true);
+        const options = wrapper.findAll("#git-branch-form-group select option");
+        expect(options.length).toBe(2);
+        expect(options.at(0).text()).toBe("master");
+        expect(options.at(0).attributes().value).toBe("master");
+        expect(options.at(1).text()).toBe("dev");
+        expect(options.at(1).attributes().value).toBe("dev");
     });
 })
--- src/app/static/src/tests/store/git/actions.test.ts
+++ src/app/static/src/tests/store/git/actions.test.ts
@@ -12,9 +12,29 @@ describe("Git actions", () => {
         mockAxios.onGet("http://app/report/run-metadata")
             .reply(200, {"data": "TEST"});
         const commit = jest.fn();
-        await actions[GitAction.FetchMetadata](mockActionContext({commit}))
+        await actions[GitAction.FetchMetadata](mockActionContext({commit}), {})
         expect(commit.mock.calls.length).toBe(1);
         expect(commit.mock.calls[0][0]).toBe(GitMutation.SetMetadata);
         expect(commit.mock.calls[0][1]).toBe("TEST")
+    })
+
+    it("selects branch and set commits", async () => {
+        mockAxios.onGet("http://app/git/branch/testbranch/commits/")
+            .reply(200, {"data": "TEST"});
+        const commit = jest.fn();
+        await actions[GitAction.SelectBranch](mockActionContext({commit}), "testbranch")
+        expect(commit.mock.calls.length).toBe(2);
+        expect(commit.mock.calls[0][0]).toBe(GitMutation.SelectBranch);
+        expect(commit.mock.calls[0][1]).toBe("testbranch")
+        expect(commit.mock.calls[1][0]).toBe(GitMutation.SetCommits);
+        expect(commit.mock.calls[1][1]).toBe("TEST")
+    })
+
+    it("selects commit", async () => {
+        const commit = jest.fn();
+        await actions[GitAction.SelectCommit](mockActionContext({commit}), "testcommit")
+        expect(commit.mock.calls.length).toBe(1);
+        expect(commit.mock.calls[0][0]).toBe(GitMutation.SelectCommit);
+        expect(commit.mock.calls[0][1]).toBe("testcommit")
     })
 });
