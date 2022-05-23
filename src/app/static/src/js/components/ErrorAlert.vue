65cb42779c8efca8f59a38c3796b4c633ef0da6e
Lekan Anifowoshe <anney.lake@gmail.com>
2022-05-23 09:42:22 +0100

    add indentation

--- src/app/static/src/js/components/ErrorAlert.vue
+++ src/app/static/src/js/components/ErrorAlert.vue
@@ -5,25 +5,25 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import {Error} from "../utils/types";
+    import Vue from "vue";
+    import {Error} from "../utils/types";
 
-interface Props {
-    error: Error
-}
-
-interface Computed {
-    message: string
-}
-
-export default Vue.extend<unknown, unknown, Computed, Props>({
-    props: {
-        "error": Object
-    },
-    computed: {
-        message: function () {
-            return this.error.message ? this.error.message : this.error.code
-        }
+    interface Props {
+        error: Error
     }
-});
+
+    interface Computed {
+        message: string
+    }
+
+    export default Vue.extend<unknown, unknown, Computed, Props>({
+        props: {
+            "error": Object
+        },
+        computed: {
+            message: function () {
+                return this.error.message ? this.error.message : this.error.code
+            }
+        }
+    });
 </script>
