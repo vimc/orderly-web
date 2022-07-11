4929f8f3eb16e4a4028514fbe4e1d05303abe5fd
Alex <alex.hill@gmail.com>
2022-07-11 12:41:44 +0100

    fix tests

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/errors/ScopeParseException.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/errors/ScopeParseException.kt
@@ -1,7 +1,3 @@
 package org.vaccineimpact.orderlyweb.errors
 
-class ScopeParseException(raw: String) : Exception(
-        "Unable to parse '$raw' as a scope. " +
-                "It should be either the global scope '*' " +
-                "or a specific scope identifier in the form 'prefix:<urlKey>'"
-)
+class ScopeParseException(raw: String) : Exception(raw)
