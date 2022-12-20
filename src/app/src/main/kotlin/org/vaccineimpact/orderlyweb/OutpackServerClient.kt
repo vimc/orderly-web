90399032f7c98189536e43929f97937b1993d9c4
Alex <alex.hill@gmail.com>
2022-12-20 11:26:19 +0000

    remove outpack client for now

--- src/app/src/main/kotlin/org/vaccineimpact/orderlyweb/OutpackServerClient.kt
+++
@@ -1,9 +0,0 @@
-package org.vaccineimpact.orderlyweb
-
-import okhttp3.OkHttpClient
-import org.vaccineimpact.orderlyweb.db.Config
-
-class OutpackServerClient(
-        config: Config,
-        client: OkHttpClient = OkHttpClient()
-) : PorcelainAPIClient("Outpack server", config["outpack.server"], client)
