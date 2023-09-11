895ae19a6415cd43a1d5c08ccd5d803f83d7b7e6
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-09-11 12:43:47 +0100

    zip file name has changed

--- scripts/install-chromedriver.sh
+++ scripts/install-chromedriver.sh
@@ -7,7 +7,7 @@ apt-get install -y unzip xvfb libxi6 libgconf-2-4
 CHROME_DRIVER_URL=https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$(google-chrome --product-version)/chromedriver-linux64.zip
 echo Fetching from $CHROME_DRIVER_URL
 curl -O $CHROME_DRIVER_URL
-unzip chromedriver_linux64.zip
+unzip chromedriver-linux64.zip
 mv chromedriver /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
