0087bb1017668dab943e1190c6535a470b6a37e0
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-09-11 10:53:03 +0100

    unsilence the curl

--- scripts/install-chromedriver.sh
+++ scripts/install-chromedriver.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y unzip xvfb libxi6 libgconf-2-4
 
 # See https://chromedriver.chromium.org/downloads/version-selection
-curl -sO https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$(google-chrome --product-version | cut -d. -f1-3))/chromedriver_linux64.zip
+curl -O https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$(google-chrome --product-version | cut -d. -f1-3))/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
